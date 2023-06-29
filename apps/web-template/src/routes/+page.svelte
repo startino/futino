7e76d3e6f8033ef9a576e42fca4323a4a73a4cc8
eksno <eksno@protonmail.com>
2023-06-29 15:26:12 +0200

    set up web-template with monorepo
    Lots of dependency changes done to web-template

---
+++ apps/web-template/src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import ThemeSwitcher from '$lib/components/atoms/ThemeSwitcher.svelte';
+</script>
+
+<ThemeSwitcher />
+<main
+	class="py-40 px-4 border-b shadow-2xl sm:px-6 md:px-8 border-primary-light/40 dark:border-primary-dark/40"
+>
+	<div class="grid justify-items-center space-y-12 mx-auto text-center">
+		<h1 class="display-large">Welcome to Futino Web-Template</h1>
+		<ul>
+			<li>
+				<a class="link" href="https://futi.no">Futino</a>
+			</li>
+			<li>
+				<a class="link" href="https://web-template.futi.no">Web-Template</a>
+			</li>
+			<li>
+				<a class="link" href="https://github.com/Futino/web-template">Source Code</a>
+			</li>
+		</ul>
+	</div>
+</main>
