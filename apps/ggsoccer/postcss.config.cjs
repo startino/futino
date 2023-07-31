03a5fdb21e4bcbec0258f52530984705df22c647
eksno <eksno@protonmail.com>
2023-07-31 18:02:08 +0200

    Add GGSoccer, update deps

--- apps/ggsoccer/postcss.config.cjs
+++ apps/ggsoccer/postcss.config.cjs
@@ -1,13 +1,11 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
 
-/** @type {import('postcss-load-config').Config} */
 const config = {
 	plugins: [
-		// Some plugins, like tailwindcss/nesting, need to run before Tailwind,
+		//Some plugins, like tailwindcss/nesting, need to run before Tailwind,
 		tailwindcss(),
-		// But others, like autoprefixer, need to run after,
+		//But others, like autoprefixer, need to run after,
 		autoprefixer,
 	],
 };
