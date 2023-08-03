53f63888aca97cec61bf5d0876b7a71cf186bec8
Jonas Lindberg <eksno@protonmail.com>
2023-08-04 00:55:02 +0200

    lots of  progress

--- apps/web-template/src/lib/molecules/Header.svelte
+++ apps/web-template/src/lib/molecules/Header.svelte
@@ -3,14 +3,26 @@
 	import Logo from '$lib/atoms/Logo.svelte';
 	import { onMount } from 'svelte';
 
+  export const elements = [
+    {
+      "name": "About",
+      "href": "/",
+    }
+  ]
+
 	let scrollY: number;
-	let hasScrolled: boolean = false; // If the user has scrolled at all => true
+  let menuOpen = false;
 
 	let largeHeaderClass =
-		'py-10 text-primary-container-on-light dark:text-primary-container-on-dark';
+		'py-12';
 	let activeheaderClass = largeHeaderClass;
 	let miniHeaderClass =
-		'py-3 border-b border-secondary-light/11 dark:border-secondary-dark/10 backdrop-blur bg-surface-light/60 dark:bg-surface-dark/60 text-primary-container-on-light backdrop-blur dark:text-primary-container-on-dark';
+		'py-3 border-secondary-light/10 dark:border-secondary-dark/10 bg-surface-light/60 dark:bg-surface-dark/60 backdrop-blur';
+
+  
+  function toggleMenu() {
+    menuOpen = !menuOpen;
+  }
 
 	onMount(() => {
 		const header = document.querySelector('#header');
@@ -32,32 +44,61 @@
 
 <svelte:window bind:scrollY />
 
+{#if menuOpen}
+<div class="z-50 fixed inset-0 bg-black/20 backdrop-blur-sm">
+  <ul class="absolute top-6 right-6 p-6 w-full max-w-xs bg-primary-container-light dark:bg-primary-container-dark rounded-lg shadow-lg">
+    <button class="absolute top-6 right-6 w-7 h-7 flex items-center justify-center" on:click={toggleMenu}>
+      <svg viewBox="0 0 10 10" class="w-3 h-3 overflow-visible"><path d="M0 0L10 10M10 0L0 10" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"></path></svg>
+    </button>  
+
+
+    <li class="flex gap-6 text-lg">
+      {#each elements as element, i}
+      <a
+        class="text-primary-container-on-light dark:text-primary-container-on-dark hover:text-tertiary-light dark:hover:text-tertiary-dark"
+        href={element.href}>
+        {element.name}
+      </a>
+      {/each}
+    </li>
+  </ul>
+</div>
+
+{/if}
+
 <div
 	id="header"
-	class="{$$props.class} {activeheaderClass} border-b border-transparent fixed top-0 z-40 flex-none w-full transition-all duration-400">
-	<div class="py-2 mx-5">
-		<div class="flex relative items-center md:px-10 lg:px-20 xl:px-40 px-auto md:px-auto">
-			<a class="flex overflow-hidden px-3 gap-3 justify-center" href="/">
-				<Logo />
-				<p class="invisible pt-0.5 text-lg font-bold sm:visible">Futino</p>
-			</a>
-			<div class="flex items-center ml-auto">
-				<nav class="body-large font-semibold tracking-wide">
-					<ul class="flex m-auto space-x-8">
-						<li>
-							<a
-								class="inline hover:text-tertiary-light dark:hover:text-tertiary-dark"
-								href="/pricing">
-								About
-							</a>
-						</li>
-						<li class="border-l border-secondary-light/20 dark:border-secondary-dark/20" />
-						<li>
-							<ThemeButton />
-						</li>
-					</ul>
-				</nav>
-			</div>
-		</div>
-	</div>
+	class="{$$props.class} {activeheaderClass} text-primary-light dark:text-primary-dark fixed top-0 z-40 flex-none w-full transition-all duration-400">
+  <div class="flex relative items-center px-10 md:px-20 lg:max-w-5xl xl:max-w-6xl mx-auto">
+    <a class="flex gap-3" href="/">
+      <Logo />
+      <p class="hidden sm:flex text-xl my-auto pb-1">Futino Web-Template</p>
+    </a>
+    <div class="flex items-center ml-auto">
+      <nav class="body-large font-semibold tracking-wide">
+        <ul class="flex gap-6">
+          <li class="hidden md:flex my-auto pb-1">
+            {#each elements as element, i}
+            <a
+              class="inline hover:text-tertiary-light dark:hover:text-tertiary-dark"
+              href={element.href}>
+              {element.name}
+            </a>
+            {/each}
+          </li>
+          <li class="hidden md:flex border-l border-secondary-light/20 dark:border-secondary-dark/20" />
+          <li class="mt-1">
+            <ThemeButton />
+          </li>
+          <li class="flex md:hidden my-auto">
+            <button on:click={toggleMenu}>
+              <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
+              </svg>
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
 </div>
