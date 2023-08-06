b4d53aba681150eb1cbfcc5c114d8babb05cc584
eksno <eksno@protonmail.com>
2023-08-06 21:54:58 +0200

    Complete dependency rework

--- apps/eksno/src/routes/dnd/patch-notes/+page.svelte
+++ apps/eksno/src/routes/dnd/patch-notes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { marked } from 'marked';
+    import SvelteMarkdown from 'svelte-markdown';
 	import Header from '$lib/molecules/Header.svelte';
     import patch_notes from '$lib/patch_notes.md?raw';
 </script>
@@ -10,6 +10,6 @@
 	class="py-40 px-4 border-b shadow-2xl sm:px-6 md:px-8 border-primary-light/40 dark:border-primary-dark/40">
 	<article
 		class="text-left prose prose-sm sm:prose-base md:prose-lg lg:prose-xl xl:prose-2xl dark:prose-invert prose-main justify-items-center space-y-12 mx-auto">
-        { @html marked(patch_notes) }
+        <SvelteMarkdown source={patch_notes} />
     </article>
 </main>
