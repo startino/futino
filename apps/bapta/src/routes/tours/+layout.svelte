fd2375f8428b77d56853ba6455dfef87f1c373f3
eksno <eksno@protonmail.com>
2023-08-11 14:20:04 +0200

    remove unused imports

--- apps/bapta/src/routes/tours/+layout.svelte
+++ apps/bapta/src/routes/tours/+layout.svelte
@@ -1,8 +1,3 @@
-<script lang="ts">
-	import Footer from '$lib/components/organisms/Footer.svelte';
-	import Header from '$lib/components/organisms/Header.svelte';
-</script>
-
 <main
 	class="py-40 px-4 border-b shadow-2xl sm:px-6 md:px-8 border-primary-light/40 dark:border-primary-dark/40">
 	<article
