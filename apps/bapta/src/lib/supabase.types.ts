431ed4e53b1b0f7d36872f5ff7918b6cb8ee2dc5
Jonas Lindberg <33558637+eksno@users.noreply.github.com>
2023-08-15 02:13:58 +0200

    Bapta Supabase Integration and functionality (#74)

---
+++ apps/bapta/src/lib/supabase.types.ts
@@ -0,0 +1,77 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      Attendance: {
+        Row: {
+          created_at: string
+          day: number
+          id: number
+          month: number
+          name: string
+        }
+        Insert: {
+          created_at?: string
+          day: number
+          id?: number
+          month: number
+          name: string
+        }
+        Update: {
+          created_at?: string
+          day?: number
+          id?: number
+          month?: number
+          name?: string
+        }
+        Relationships: []
+      }
+      bapta: {
+        Row: {
+          choice: string
+          contact: string
+          created_at: string
+          id: number
+          name: string
+          people: number
+        }
+        Insert: {
+          choice: string
+          contact: string
+          created_at?: string
+          id?: number
+          name: string
+          people: number
+        }
+        Update: {
+          choice?: string
+          contact?: string
+          created_at?: string
+          id?: number
+          name?: string
+          people?: number
+        }
+        Relationships: []
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
---
+++ apps/bapta/src/routes/admin/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { createClient } from '@supabase/supabase-js';
+	import type { Database } from '$lib/supabase.types';
+
+	// Create a Supabase client object
+	const supabase = createClient<Database>(
+		'https://ekushmqbxanusqyxyzkk.supabase.co',
+		'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrdXNobXFieGFudXNxeXh5emtrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODEzODQ1NzMsImV4cCI6MTk5Njk2MDU3M30.RTlWWjf6n2PCDkMdN43B0lBjsXe_aEIfTIs2bDCxfSE',
+	);
+</script>
+
+<div class="flex flex-col w-full h-full shadow-lg shadow-background-dark lg:grid-cols-2 card-shadow grid-rows-2 px-10 lg:px-20 xl:px-40 py-40 justify-center place-items-center border-b border-secondary-light/40 dark:border-secondary-dark/40 bg-[url('/images/safari/18.jpeg')] bg-cover bg-left">
+	<h1 class="font-bold text-primary-dark display-medium">Admin Board</h1>
+
+	<hr class="w-40 py-4" />
+
+	<div class="grid grid-cols-1 md:grid-cols-2 place-items-center gap-5">
+    {#await supabase.from('bapta').select('*')}
+      <p>Loading...</p>
+    {:then bapta}
+      {#if bapta.data}
+        {#each bapta.data as tour}
+				  <div class="rounded-lg shadow-lg shadow-black bg-surface-dark/70 h-full">
+            <div class="p-5 text-surface-variant-on-dark prose prose-sm sm:prose-base md:prose-lg lg:prose-xl xl:prose-2xl dark:prose-invert prose-main justify-items-center mx-auto">
+              <p>Full Name: {tour.name}</p>
+              <p>Tour: {tour.choice}</p>
+              <p>Contact: {tour.contact}</p>
+              <p>People: {tour.people}</p>
+              <p>Request Date: {tour.created_at}</p>
+            </div>
+          </div>
+        {/each}
+      {/if}
+    {/await}
+  </div>
+</div>
+
