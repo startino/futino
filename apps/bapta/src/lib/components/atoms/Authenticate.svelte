b58184383bf43dd334d5ebca97d416331a8962fe
Jonas Lindberg <33558637+eksno@users.noreply.github.com>
2023-08-16 00:41:58 +0200

    Bapta Authentication (#75)

---
+++ apps/bapta/src/lib/components/atoms/Authenticate.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+	import sha256 from 'crypto-js/sha256';
+	import Button from '$lib/components/atoms/Button.svelte';
+
+	// Double SHA256 hashed
+	const key = 'd17b0d9a8b44aaa69bcbd82285e67a30418a6c3dc38e16c02e64035386452580';
+
+	// Debug value, should login instance be remembered?
+	const rememberKey = false;
+
+	function reload() {
+		window.location.reload();
+	}
+
+	function verify() {
+		// Remove key from localStorage the key for debug purposes.
+		if (!rememberKey) localStorage.setItem('key', '');
+
+		// Check if the saved key in localStorage is correct, if so verify immediately.
+		if (sha256(localStorage.getItem('key')) == key) return true;
+
+		// First hashing with SHA256 so the original password cannot be read in localStorage
+		// A second hashing of the input is performed only on checks to prevent copy pasting
+		// the true key.
+		localStorage.setItem('key', sha256(prompt('Enter Key', '')?.replace(/\s+/g, '')));
+
+    // Log given hashes of input key.
+    console.debug(`SHA256: ${localStorage.getItem('key')}\nDouble SHA256: ${sha256(localStorage.getItem('key'))}`);
+
+		// Perform second hashing only when checking to prevent user from edeting their
+		// localStorage to ensure they didn't copy paste the true key.
+		return sha256(localStorage.getItem('key')) == key;
+	}
+
+	let verified: Promise<boolean> = new Promise((resolve, reject) => {
+		try {
+			resolve(verify());
+		} catch {
+			reject('Error with verification.');
+		}
+	});
+</script>
+
+{#await verified}
+  <main class="h-screen grid grid-rows-3">
+    <div class="grid row-start-2 space-y-12 place-items-center">
+      <h1 class="text-6xl">Authenticating...</h1>
+    </div>
+  </main>
+{:then isVerified}
+	{#if isVerified}
+    <slot/>
+	{:else}
+		<main class="h-screen grid grid-rows-3">
+			<div class="grid row-start-2 space-y-12 place-items-center">
+				<h1 class="text-6xl">Failed authentication</h1>
+        <h2 class="text-2xl">Please Refresh Page and enter the right key</h2>
+				<button on:click={reload}>
+					<Button>Refresh Page</Button>
+				</button>
+			</div>
+		</main>
+	{/if}
+{/await}
+
--- apps/bapta/src/routes/admin/+page.svelte
+++ apps/bapta/src/routes/admin/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { createClient } from '@supabase/supabase-js';
+  import Authenticate from '$lib/components/atoms/Authenticate.svelte';
 	import type { Database } from '$lib/supabase.types';
 
 	// Create a Supabase client object
@@ -9,29 +10,30 @@
 	);
 </script>
 
-<div class="flex flex-col w-full h-full shadow-lg shadow-background-dark lg:grid-cols-2 card-shadow grid-rows-2 px-10 lg:px-20 xl:px-40 py-40 justify-center place-items-center border-b border-secondary-light/40 dark:border-secondary-dark/40 bg-[url('/images/safari/18.jpeg')] bg-cover bg-left">
-	<h1 class="font-bold text-primary-dark display-medium">Admin Board</h1>
+<Authenticate>
+  <div class="flex flex-col w-full h-full shadow-lg shadow-background-dark lg:grid-cols-2 card-shadow grid-rows-2 px-10 lg:px-20 xl:px-40 py-40 justify-center place-items-center border-b border-secondary-light/40 dark:border-secondary-dark/40 bg-[url('/images/safari/18.jpeg')] bg-cover bg-left">
+    <h1 class="font-bold text-primary-dark display-medium">Admin Board</h1>
 
-	<hr class="w-40 py-4" />
+    <hr class="w-40 py-4" />
 
-	<div class="grid grid-cols-1 md:grid-cols-2 place-items-center gap-5">
-    {#await supabase.from('bapta').select('*')}
-      <p>Loading...</p>
-    {:then bapta}
-      {#if bapta.data}
-        {#each bapta.data as tour}
-				  <div class="rounded-lg shadow-lg shadow-black bg-surface-dark/70 h-full">
-            <div class="p-5 text-surface-variant-on-dark prose prose-sm sm:prose-base md:prose-lg lg:prose-xl xl:prose-2xl dark:prose-invert prose-main justify-items-center mx-auto">
-              <p>Full Name: {tour.name}</p>
-              <p>Tour: {tour.choice}</p>
-              <p>Contact: {tour.contact}</p>
-              <p>People: {tour.people}</p>
-              <p>Request Date: {tour.created_at}</p>
+    <div class="grid grid-cols-1 md:grid-cols-2 place-items-center gap-5">
+      {#await supabase.from('bapta').select('*')}
+        <p>Loading...</p>
+      {:then bapta}
+        {#if bapta.data}
+          {#each bapta.data as tour}
+            <div class="rounded-lg shadow-lg shadow-black bg-surface-dark/70 h-full">
+              <div class="p-5 text-surface-variant-on-dark prose prose-sm sm:prose-base md:prose-lg lg:prose-xl xl:prose-2xl dark:prose-invert prose-main justify-items-center mx-auto">
+                <p>Full Name: {tour.name}</p>
+                <p>Tour: {tour.choice}</p>
+                <p>Contact: {tour.contact}</p>
+                <p>People: {tour.people}</p>
+                <p>Request Date: {tour.created_at}</p>
+              </div>
             </div>
-          </div>
-        {/each}
-      {/if}
-    {/await}
+          {/each}
+        {/if}
+      {/await}
+    </div>
   </div>
-</div>
-
+</Authenticate>
