cd8cbdd318971d535f39b1871a530046a612399e
Jorge Lewis <antopiahk@gmail.com>
2023-08-19 00:00:40 +0800

    header working with animations and colors

--- apps/bapta/src/lib/components/organisms/Header.svelte
+++ apps/bapta/src/lib/components/organisms/Header.svelte
@@ -1,32 +1,36 @@
 <script lang="ts">
-	import { base } from '$app/paths';
 	import ThemeButton from '$lib/components/atoms/ThemeButton.svelte';
 	import Logo from '$lib/components/atoms/Logo.svelte';
 	import { onMount } from 'svelte';
-	import { slide } from 'svelte/transition';
-	import Icon from '../atoms/Icon.svelte';
+	import { fade, slide } from 'svelte/transition';
 
-	let adventuresOpen = false;
-	function mouseOver() {
-		adventuresOpen = true;
-		console.log('entered');
-	}
-	function overover() {
-		adventuresOpen = true;
-		console.log('overover');
-	}
+	export const elements = [
+		{
+			name: 'Home',
+			href: '/',
+		},
+		{
+			name: 'Contact',
+			href: '/contact',
+		},
+		{
+			name: 'Tours',
+			href: '/tours',
+		},
+		{
+			name: 'Adventures',
+			href: '/adventures',
+		},
+		{
+			name: 'About',
+			href: '/about',
+		},
+	];
 
-	function mouseLeave() {
-		adventuresOpen = false;
-		console.log('leaved');
-	}
-	function focus() {
-		//isHovered = true;
-	}
-
-	let largeHeaderClass = 'fixed py-12';
+	let largeHeaderClass = 'fixed text-primary-dark py-8 md:bg-surface-dark/60 md:backdrop-blur';
 	let activeheaderClass = largeHeaderClass;
-	let miniHeaderClass = 'fixed py-3 border-b bg-surface-dark/60 backdrop-blur';
+	let miniHeaderClass =
+		'fixed py-4 border-b text-primary-dark bg-surface-dark/60 bg-surface-dark/60 backdrop-blur';
 
 	let menuOpen = false;
 	function toggleMenu() {
@@ -40,9 +44,49 @@
 	});
 </script>
 
+{#if menuOpen}
+	<!-- svelte-ignore a11y-no-static-element-interactions -->
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
+	<!-- Background Blurr -->
+	<div transition:fade class="fixed inset-0">
+		<div on:click={toggleMenu} class="bg-black/50 absolute w-screen h-screen backdrop-blur-sm" />
+		<!-- Menu Card -->
+		<div
+			in:slide={{ delay: 200, duration: 300 }}
+			out:slide
+			class="absolute z-50 p-6 w-full mx-auto pb-8 my-auto top-0 bottom-0 flex flex-col items-center gap-4 left-0 right-0 h-fit max-w-xs md:max-w-xl lg:max-w-2xl bg-surface-dark rounded-lg shadow-lg">
+			<!-- Close Menu Button -->
+			<button
+				class="w-7 h-7 my-4 flex items-center hover:scale-150 transition-all duration-200 justify-center stroke-primary-dark hover:stroke-tertiary-dark"
+				on:click={toggleMenu}>
+				<svg viewBox="0 0 10 10" class="w-3 h-3 overflow-visible"
+					><path
+						d="M0 0L10 10M10 0L0 10"
+						fill="none"
+						stroke-width="2"
+						stroke-linecap="round" /></svg>
+			</button>
+
+			<!-- Nav Elements -->
+			<nav class="text-center flex flex-col gap-6 headline-medium">
+				{#each elements as element}
+					<a
+						class="text-primary-container-on-dark hover:text-tertiary-dark hover:scale-105 transition-all duration-200"
+						href={element.href}
+						on:click={toggleMenu}>
+						{element.name}
+					</a>
+				{/each}
+			</nav>
+		</div>
+	</div>
+{/if}
+
 <header
 	id="header"
-	class="{$$props.class} {activeheaderClass} text-primary-light dark:text-primary-dark border-secondary-light/10 dark:border-secondary-dark/10 top-0 z-40 w-full transition-all duration-400">
+	class="{$$props.class} {activeheaderClass} {menuOpen
+		? 'opacity-0'
+		: 'opacity-100'}  border-secondary-dark/10 top-0 z-40 w-full transition-all duration-200">
 	<div class="flex relative items-center px-6 md:px-18 lg:max-w-5xl xl:max-w-6xl mx-auto">
 		<a class="flex gap-3" href="/">
 			<Logo />
@@ -50,59 +94,15 @@
 		</a>
 		<div class="flex items-center ml-auto gap-6">
 			<nav class="hidden md:flex gap-6 pb-0.5">
-				<a class="nav-item" href="/">Home </a>
-
-				<a class="nav-item" href="/tours"> Tours </a>
-				<a class="nav-item" href="/contact"> Contact </a>
-				<div class="relative group">
-					<a
-						class="flex flex-row items-center gap-x-1 hover:text-tertiary-light dark:hover:text-tertiary-dark"
-						href="/adventures">
-						<h1 class="">Adventures</h1>
-						<span class="inline-flex self-center">
-							<Icon
-								class="group-hover:translate-y-1 transition-transform group-hover:rotate-180 group-hover:-translate-x-1 justify-self-center self-center pr-1 nav-item"
-								fillColor="currentColor"
-								icon="down-caret"
-								height="12"
-								width="12" />
-						</span>
+				{#each elements as element, i}
+					<a class=" hover:text-tertiary-dark transition-all duration-300" href={element.href}>
+						{element.name}
 					</a>
-
-					<div class="mt-2 overflow-hidden h-fit">
-						<div class=" absolute top-0 w-full h-8" />
-						<ul
-							class="absolute invisible group-hover:visible -translate-y-12 opacity-0 group-hover:opacity-100 group-hover:translate-y-0 hover:visible transition-all duration-500 flex flex-col text-left py-2 body-small bg-surface-dark/50 outline outline-1 pt-2 rounded-lg gap-y-2 [&>*]:px-2">
-							<li>
-								<a
-									class="inline hover:text-tertiary-light dark:hover:text-tertiary-dark"
-									href="/adventures/kilimanjaro">
-									Mount Kilimanjaro
-								</a>
-							</li>
-							<li>
-								<a
-									class="inline hover:text-tertiary-light dark:hover:text-tertiary-dark"
-									href="/adventures/kilimanjaro">
-									Mount Meru
-								</a>
-							</li>
-							<li>
-								<a
-									class="inline hover:text-tertiary-light dark:hover:text-tertiary-dark"
-									href="/adventures/zanzibar">
-									Zanzibar Beach
-								</a>
-							</li>
-						</ul>
-					</div>
-				</div>
-				<a href="/about" class="nav_item"> About</a>
+				{/each}
 			</nav>
-			<div
-				class="gap-6 pl-6 flex md:border-l md:border-secondary-light/50 md:dark:border-primary-dark/50">
+			<div class="flex md:border-primary-dark/50">
 				<button
-					class="flex md:hidden stroke-primary-light hover:stroke-tertiary-light dark:stroke-primary-dark dark:hover:stroke-tertiary-dark"
+					class="flex md:hidden stroke-primary-dark hover:stroke-tertiary-dark transition-all"
 					on:click={toggleMenu}>
 					<svg class="w-6 h-6" fill="none" viewBox="0 0 24 24">
 						<path
@@ -116,9 +116,3 @@
 		</div>
 	</div>
 </header>
-
-<style>
-	.nav-item {
-		@apply hover:text-tertiary-light dark:hover:text-tertiary-dark;
-	}
-</style>
