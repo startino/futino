f575e1a20d6ec6279d35284c3950270fa2fdbf87
Jorge Lewis <antopiahk@gmail.com>
2023-08-28 19:49:36 +0800

    inserting the iframe from cal.com

--- apps/website/src/routes/booking/+page.svelte
+++ apps/website/src/routes/booking/+page.svelte
@@ -1,1 +1,51 @@
-this page is in progress...
+<script lang="js" type="text/javascript">
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		(function (C, A, L) {
+			let p = function (a, ar) {
+				a.q.push(ar);
+			};
+			let d = C.document;
+			C.Cal =
+				C.Cal ||
+				function () {
+					let cal = C.Cal;
+					let ar = arguments;
+					if (!cal.loaded) {
+						cal.ns = {};
+						cal.q = cal.q || [];
+						d.head.appendChild(d.createElement('script')).src = A;
+						cal.loaded = true;
+					}
+					if (ar[0] === L) {
+						const api = function () {
+							p(api, arguments);
+						};
+						const namespace = ar[1];
+						api.q = api.q || [];
+						typeof namespace === 'string' ? (cal.ns[namespace] = api) && p(api, ar) : p(cal, ar);
+						return;
+					}
+					p(cal, ar);
+				};
+		})(window, 'https://app.cal.com/embed/embed.js', 'init');
+		Cal('init', { origin: 'https://cal.com' });
+
+		Cal('inline', {
+			elementOrSelector: '#my-cal-inline',
+			calLink: 'jorgelewis/30min',
+			layout: 'month_view',
+		});
+
+		Cal('ui', {
+			cssVarsPerTheme: { branding: { brandColor: '#FFF' } },
+			hideEventTypeDetails: false,
+			layout: 'month_view',
+		});
+	});
+</script>
+
+<!-- Cal inline embed code begins -->
+<div style="width:100%;height:100%;overflow:scroll" id="my-cal-inline" />
+<!-- Cal inline embed code ends -->
