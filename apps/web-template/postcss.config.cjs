1491996dc8828a5211ac26fe5f32365ecd6da881
eksno <eksno@protonmail.com>
2023-09-03 16:43:01 +0200

    fix files

--- apps/web-template/postcss.config.cjs
+++ apps/web-template/postcss.config.cjs
@@ -1,17 +1,16 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const path = require('path');
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
 
 /** @type {import('postcss-load-config').Config} */
 const config = {
 	plugins: [
-		require('postcss-import'),
 		// Some plugins, like tailwindcss/nesting, need to run before Tailwind,
-		tailwindcss(path.resolve(__dirname, './tailwind.config.js')),
+		tailwindcss(),
 		// But others, like autoprefixer, need to run after,
 		autoprefixer,
 	],
 };
 
 module.exports = config;
+
