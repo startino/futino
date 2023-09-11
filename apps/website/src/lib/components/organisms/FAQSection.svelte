4a95669481eb7192a8ad66099fa3acfb161ac43e
Jorge Lewis <antopiahk@gmail.com>
2023-09-11 16:50:30 +0800

    adapting faq section to website

--- apps/website/src/lib/components/organisms/FAQSection.svelte
+++ apps/website/src/lib/components/organisms/FAQSection.svelte
@@ -5,18 +5,16 @@
 
 	// Gets the Frequently Asked Questions by parent component
 	export let faqs: {
-		index: number;
 		question: string;
 		answer: string;
 	}[] = [];
-	let activeEntry: number;
+	let questionEntry: string;
 
-	const handleClick = (questionIndex: number) => () => (activeEntry = questionIndex);
+	const handleClick = (question: string) => () => (questionEntry = question);
 
 	// For Search Input
 	let searchInput = '';
 	let filteredFaqs: {
-		index: number;
 		question: string;
 		answer: string;
 	}[] = [];
@@ -30,40 +28,38 @@
 	};
 </script>
 
-<section class="overflow-hidden relative">
-	<div
-		class="absolute bottom-0 w-screen bg-gradient-to-t from-background-dark to-transparent h-36" />
-	<div class="inner-section grid grid-cols-1 md:grid-cols-3 items-start gap-3 h-[5000px]">
+<section class="">
+	<div class="inner-section grid grid-cols-1 md:grid-cols-5 gap-3 h-screen">
 		<div
-			class="col-span-1 flex flex-col gap-y-4 items-center self-start md:items-start md:text-left text-surface-on-dark">
+			class="col-span-2 h-fit flex flex-col gap-y-4 items-center self-start md:items-start md:text-left text-surface-on-dark">
 			<h1 class="font-semibold headline-medium md:display-medium leading-tight">
-				Your Questions, Answered
+				Your Questions, <br /> Answered
 			</h1>
-			<h1 class="body-large hidden md:flex">Find Clarity and Confidence.</h1>
-
-			<Button href="/contact" class="py-2">
-				<h1 class="font-bold title-small">See More</h1>
-			</Button>
+			<h1 class="title-medium hidden md:flex">
+				Find Clarity and Confidence <br /> through some frequently asked questions.
+			</h1>
 		</div>
-		<div class="flex flex-col gap-4 h-screen col-span-2 items-center">
-			<SearchBox class="w-full mb-4" bind:searchInput on:input={searchPhotos} />
-
-			{#if searchInput && filteredFaqs.length === 0}
-				<section>
-					<div class="w-full py-12 display-small inline">
-						<h1 class="">Sorry, we couldn't find your question.</h1>
-						<a href="/contact" class="underline text-tertiary-dark">Ask Us Here!</a>
-					</div>
-				</section>
-			{:else if filteredFaqs.length > 0}
-				{#each filteredFaqs as { question, answer, index }}
-					<FAQCard {question} {answer} isOpen={index == activeEntry} />
-				{/each}
-			{:else}
-				{#each faqs as { question, answer, index }}
-					<FAQCard {question} {answer} isOpen={index == activeEntry} />
-				{/each}
-			{/if}
+		<div class="flex flex-col gap-4 md:col-span-3 self-start w-full">
+			<div class="lex flex-col gap-4 items-start">
+				<SearchBox class="w-full mb-4 md:mb-6" bind:searchInput on:input={searchPhotos} />
+				<div class="flex flex-col gap-4 overflow-scroll w-full max-h-[505px]">
+					{#if searchInput && filteredFaqs.length === 0}
+						<div class="w-full py-12 headline-medium inline">
+							<h1 class="">Sorry, we couldn't find your question.</h1>
+							<a href="/contact" class="underline text-tertiary-dark headline-small"
+								>Ask Us Here!</a>
+						</div>
+					{:else if filteredFaqs.length > 0}
+						{#each filteredFaqs as { question, answer }}
+							<FAQCard {question} {answer} isOpen={question == questionEntry} />
+						{/each}
+					{:else}
+						{#each faqs as { question, answer }}
+							<FAQCard {question} {answer} isOpen={question == questionEntry} />
+						{/each}
+					{/if}
+				</div>
+			</div>
 		</div>
 	</div>
 </section>
