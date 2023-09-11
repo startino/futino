5688ec05778aca59ecfb0613c75e1139a819ab5c
Jorge Lewis <antopiahk@gmail.com>
2023-09-11 13:52:51 +0800

    mobile nav menu working properly

--- apps/website/src/lib/components/organisms/ChapterMenu.svelte
+++ apps/website/src/lib/components/organisms/ChapterMenu.svelte
@@ -29,42 +29,39 @@
 	}[] = [];
 </script>
 
-<div class="fixed z-40 flex w-fit left-1/2 -translate-x-1/2 bottom-0 p-6">
+<div class="fixed z-40 flex w-full bottom-0 p-6">
 	<button
 		on:click={handleExpandClick}
-		class="flex flex-row md:hidden gap-6 max-w-xs bg-surface-light dark:bg-surface-dark rounded-xl p-4 text-left items-center w-52">
-		<Logo class="self-end" />
+		class="flex flex-row justify-items-start md:hidden gap-6 bg-surface-dark border-1 border-primary-dark rounded-md items-center max-w-xs w-full mr-32 px-4 py-3">
+		<Logo class={isExpanded ? 'self-end -translate-y-1' : 'self-center'} />
 
-		<div class="flex flex-col transition-all duration-1000">
-			<ul class="divide-y-1 divide-black/10 dark:divide-white/10 space-y-4">
-				{#if isExpanded}
-					{#each chapters as { chapterNumber, title, href }}
-						<li class="">
-							<a on:click={() => (chapterInView = chapterNumber)} href="#{href}">
-								{#if chapterNumber === chapterInView}
-									<!--The Selected Chapter-->
-									<h1 class="body-small text-left text-primary-light dark:text-primary-dark">
-										Chapter 0{chapterNumber}
-									</h1>
-									<h1 class="body-large text-left font-bold">{title}</h1>
-								{:else}
-									<!--Non-selected Chapters-->
-									<h1 class="body-small text-left">Chapter 0{chapterNumber}</h1>
-									<h1 class="body-large text-left font-bold">{title}</h1>
-								{/if}
-							</a>
-						</li>
-					{/each}
-				{:else}
-					<!--Selected chapter when not collapsed-->
-					<div class="flex flex-col">
-						<h1 class="title-small text-primary-light dark:text-primary-dark">
-							Chapter 0{chapters[chapterInView].chapterNumber}
-						</h1>
-						<h1 class="title-medium font-bold">{chapters[chapterInView].title}</h1>
+		<div class="flex flex-col gap-2 transition-all duration-1000 text-left w-full">
+			{#if isExpanded}
+				{#each chapters as { chapterNumber, title, href }, i}
+					<div
+						class="pl-1 {i + 1 != chapters.length ? 'border-b border-outline-dark/50 pb-2 ' : ''}">
+						<a on:click={() => (chapterInView = chapterNumber)} {href}>
+							{#if chapterNumber === chapterInView}
+								<!--The Selected Chapter-->
+								<h1 class="body-small font-extrabold text-tertiary-dark">0{chapterNumber}</h1>
+								<h1 class="body-large font-bold text-primary-dark">{title}</h1>
+							{:else}
+								<!--Non-selected Chapters-->
+								<h1 class="body-small font-extrabold text-tertiary-dark">0{chapterNumber}</h1>
+								<h1 class="body-large">{title}</h1>
+							{/if}
+						</a>
 					</div>
-				{/if}
-			</ul>
+				{/each}
+			{:else}
+				<!--Selected chapter when not collapsed-->
+				<div class="flex flex-col pl-1">
+					<h1 class="title-small text-tertiary-dark">
+						0{chapters[chapterInView].chapterNumber}
+					</h1>
+					<h1 class="title-medium font-bold">{chapters[chapterInView].title}</h1>
+				</div>
+			{/if}
 		</div>
 	</button>
 	<!--Chapter Menu for md+-->
