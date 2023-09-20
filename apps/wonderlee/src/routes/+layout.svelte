1f86affee27563da7b45853c6c5c3d9f20429004
Jorge Lewis <antopiahk@gmail.com>
2023-09-20 12:17:24 +0800

    cherry picking lorc and wonderlee src files

--- apps/wonderlee/src/routes/+layout.svelte
+++ apps/wonderlee/src/routes/+layout.svelte
@@ -1,114 +1,16 @@
 <script lang="ts">
 	import '../app.postcss';
-	import { Header, Shell, Footer, Logo, ThemeButton, Icon } from 'lorc';
-
-	const headerElements: { name: string; href: string }[] = [
-		{
-			name: 'Home',
-			href: '/',
-		},
-		{
-			name: 'About',
-			href: '/',
-		},
-		{
-			name: 'Pricing',
-			href: '/',
-		},
-		{
-			name: 'Contact',
-			href: '/',
-		},
-	];
-
-	const footerElements: { name: string; href: string }[] = {
-		Home: '/',
-		About: '/',
-		Contact: '/',
-		Pricing: '/',
-		Contact: '/',
-	};
-
-	const fatFooterElements = {
-		Explore: [
-			{
-				name: 'Home',
-				href: '/',
-			},
-			{
-				name: 'About',
-				href: '/',
-			},
-			{
-				name: 'Pricing',
-				href: '/',
-			},
-		],
-		Legal: [
-			{
-				name: 'Privacy Policy',
-				href: '/',
-			},
-			{
-				name: 'Terms of Service',
-				href: '/',
-			},
-			{
-				name: 'Company',
-				href: '/',
-			},
-		],
-		Support: [
-			{
-				name: 'Docs',
-				href: '/',
-			},
-			{
-				name: 'Status',
-				href: '/',
-			},
-			{
-				name: 'Skills',
-				href: '/',
-			},
-			{
-				name: 'FAQs',
-				href: '/',
-			},
-		],
-		Company: [
-			{
-				name: 'Blog',
-				href: '/',
-			},
-			{
-				name: 'Status',
-				href: '/',
-			},
-			{
-				name: 'Careers',
-				href: '/',
-			},
-			{
-				name: 'FAQs',
-				href: '/',
-			},
-		],
-	};
+	import { Header, Shell, FatFooter } from 'lorc';
 </script>
 
-<Shell
-	class="card h-full p-1"
-	slotSidebarLeft="grid grid-cols-1"
-	slotSidebarRight="grid grid-cols-1"
-	slotPageContent="grid grid-cols-1">
+<Shell class="bg-background-light dark:bg-background-dark">
 	<svelte:fragment slot="header">
-		<Header pages={headerElements} centerHeader={true} companyName="Futino Web-template" />
+		<Header companyName="Wonderlee" />
 	</svelte:fragment>
 
 	<slot />
 
 	<svelte:fragment slot="footer">
-		<Footer pages={fatFooterElements} fatFooter={true} companyName="Futino Web-template" />
+		<FatFooter companyName="Wonderlee" />
 	</svelte:fragment>
 </Shell>
