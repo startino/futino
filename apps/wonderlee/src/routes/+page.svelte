ef0d445c6b3b75f4dc2cab6413d7fd9be341117e
Jorge Lewis <antopiahk@gmail.com>
2023-09-26 19:27:34 +0800

    cleaning wonderlee and updating lorc index

--- apps/wonderlee/src/routes/+page.svelte
+++ apps/wonderlee/src/routes/+page.svelte
@@ -1,9 +1,27 @@
-<script>
-  import { Prose, Hero } from "lorc";
+<script lang="ts">
+  import { Hero, Prose, Button } from "lorc";
+
+  const heroBg = "/pre_mockup_designs/garage_ref.png";
+
+  const CTAButtons = {
+    "Our work": {
+      href: "/",
+      secondary: false,
+    },
+    "Contact us": {
+      href: "/",
+      secondary: true,
+    },
+  };
 </script>
 
-<Hero
-  title="Wonderlee Title"
-  subtitle="this is a perfect subtitle"
-  bgImg="/pre_mockup_designs/wonderlee_logo.png"
-/>
+<Prose>
+  <Hero
+    {CTAButtons}
+    title="Lorem ipsum, dolor sit amet consectetur adipisicing."
+    subtitle="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel odio mollitia excepturi laudantium nostrum sit!"
+    justified="center"
+    bgImg={heroBg}
+  />
+</Prose>
+
