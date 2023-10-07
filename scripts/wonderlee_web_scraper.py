1110bbc138bbba1d07ae70ead031e7d2db8d11b0
Jorge Lewis <antopiahk@gmail.com>
2023-10-07 15:39:13 +0800

    web scrapped all projects

--- scripts/wonderlee_web_scraper.py
+++ scripts/wonderlee_web_scraper.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import os
 
 url = "https://wonderlee.hk/job_ref.html"
 response = requests.get(url)
@@ -11,25 +12,47 @@ if response.status_code == 200:
     soup = BeautifulSoup(response.text, 'html.parser')
     
     # Locate the elements containing project information
-    projects = soup.find_all('table', style="width: 87%")
+    projects = soup.find_all('tr')
 
     for project in projects:
-        print(project.prettify())
+        # print(project.prettify())
         try:
+            # image processing
+            img = project.find(attrs={'width':"200"})
+            thumbnail = re.findall(r'src="([^"]+)"', str(img))[0]
+            link = f'https://wonderlee.hk/{thumbnail}'
+            output_file = f'../apps/wonderlee/static/projects/{thumbnail.replace("%","_")}'
+            with open(output_file, "xb") as f:
+                f.write(requests.get(link).content)
+            # text processing
+            project = project.find('table')
             # Extract project title
             title = project.find('h5').text.strip()
+            # Extract project date
+            date = re.findall(r'\(.*?\)', title)[0]
+            title = title.replace(date, '').replace(':', '').strip()
+            id = title.lower().replace(" ","-").replace("'", "")
 
             # Extract project date
-            date = re.findall(r'\(.*?\)', title)
+            date = date.replace("(", "").replace(")", "")
             # Extract project description
-            description = project.find('ul').text.strip()
-
-            # Print or store the extracted data as needed
-            print(f"Title: {title}")
-            print(f"Date: {date}")
-            print(f"Description: {description}")
-        except:
-            print("")
+            description = project.find_all('li')
+            for i in range(0, len(description)):
+                description[i] = description[i].text.strip().replace('\n', '')
+                
+            #Print or store the extracted data as needed
+            #print(f''' '{id}': new Project({{
+            #name: "{title}",
+            #date: {date},
+            #thumbnail: "{thumbnail}",
+            #description : [
+            #      {str(description).replace('[', '').replace(']', '')}
+            #]
+            #}}),
+            #''')
+        except Exception as e:
+            # print(e)
+            needs=''
 else:
     print("Failed to retrieve the webpage.")
 
