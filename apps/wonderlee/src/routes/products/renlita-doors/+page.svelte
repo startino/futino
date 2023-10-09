5fc1c4bbfd127ddc13a286b294cd074c1a7819eb
Jorge Lewis <antopiahk@gmail.com>
2023-10-09 17:16:30 +0800

    avon barriers pages. all product pages done.

--- apps/wonderlee/src/routes/products/renlita-doors/+page.svelte
+++ apps/wonderlee/src/routes/products/renlita-doors/+page.svelte
@@ -5,27 +5,65 @@
   const renlita_doors = [
     {
       label: "Renlita Floataway",
-      href: "#floataway",
+      id: "floataway",
+      description: [
+        " Balanced by counter-weight, the whole door goes up as if float-away.",
+        "Built by Renlita USA according to ANSI standard.",
+        "Maximum 32' wide and 20' high.",
+        "Powered with electrically operated opener for remote control access.",
+      ],
     },
     {
       label: "Renlita Hingeway",
-      href: "#hingeway",
+      id: "hingeway",
+      description: [
+        " Balanced by counter-weight, the whole door goes up as if fold-away.",
+        "Built by Renlita USA according to ANSI standard.",
+        "Maximum 32' wide and 20' high.",
+        "Powered with electrically operated opener for remote control access.",
+      ],
     },
     {
       label: "Renlita Foldaway",
-      href: "#foldaway",
+      id: "foldaway",
+      description: [
+        "Balanced by counter-weight, the whole door goes up as if hinge-away.",
+        "Built by Renlita USA according to ANSI standard.",
+        "Maximum 90' wide and 30' high.",
+        "Designed to wind load of 10 PSF.",
+        "Powered with electrically operated opener for remote control access.",
+      ],
     },
     {
       label: "Renlita A750 NuFold",
-      href: "#nufold",
+      id: "nufold",
+      description: [
+        "Balanced by counter-weight, the whole door goes up without interior projection.",
+        "Built by Renlita USA according to ANSI standard.",
+        "Maximum 18' wide and 12' high.",
+        "Designed to wind load of 30 PSF.",
+        "Powered with electrically operated opener for remote control access.",
+      ],
     },
     {
       label: " Renlita Sovereign",
-      href: "#sovereign",
+      id: "sovereign",
+      description: [
+        "Also known as stacking door, all panels stack up to the ceiling.",
+        "Built by Renlita USA according to ANSI standard.",
+        "Maximum 16' wide and 12' high.",
+        "Powered with electrically operated opener for remote control access.",
+      ],
     },
     {
       label: " Renlita Frameless Glass",
-      href: "#frameless-glass",
+      id: "frameless-glass",
+      description: [
+        "Elegant window wall with no floor track.",
+        "Slide neatly into stacking bay.",
+        "Maximum 12' high.",
+        "Powered with electrically operated opener for remote control access.",
+      ],
     },
   ];
 </script>
@@ -34,138 +72,42 @@
   <Container class="flex flex-col">
     <h2>Different Renlita Doors</h2>
     <div class="grid grid-cols-3 gap-6 place-items-center">
-      <div class="flex flex-col place-items-center">
+      {#each renlita_doors as { label, id }}
+        <div class="flex flex-col place-items-center">
+          <img
+            src="/products/renlita-door/{id}-1.jpg"
+            alt=""
+            class="object-contain object-center w-full h-full"
+          />
+          <Button href="#{id}" class="w-full">{label}</Button>
+        </div>
+      {/each}
+    </div>
+  </Container>
+  {#each renlita_doors as { label, id, description }}
+    <Container class="flex flex-col md:grid md:grid-cols-2 gap-6" id="uphill">
+      <div class="grid grid-cols-2 md:grid-cols-1 not-prose">
         <img
-          src="/products/residential-gates/sliding/uphill_button.gif"
+          src="/products/renlita-door/{id}-1.jpg"
           alt=""
           class="object-contain object-center w-full h-full"
         />
-        <Button href="#uphill" class="w-full">Uphill</Button>
-      </div>
-      <div class="flex flex-col place-items-center">
         <img
-          src="/products/residential-gates/sliding/SSW-110-1.gif"
+          src="/products/renlita-door/{id}-2.jpg"
           alt=""
           class="object-contain object-center w-full h-full"
         />
-        <Button href="#curved" class="w-full">Curved</Button>
       </div>
-
-      <div class="flex flex-col place-items-center">
-        <img
-          src="/products/residential-gates/sliding/Telescopic_button.gif"
-          alt=""
-          class="object-contain object-center w-full h-full"
-        />
-        <Button href="#telescopic" class="w-full">Telescopic</Button>
+      <div class="flex flex-col col-span-1">
+        <h2>{label}</h2>
+        <ul class="ulink">
+          {#each description as item}
+            <li>
+              {item}
+            </li>
+          {/each}
+        </ul>
       </div>
-
-      <div class="flex flex-col place-items-center">
-        <img
-          src="/products/residential-gates/sliding/Cantilever_gate_2.gif"
-          alt=""
-          class="object-contain object-center w-full h-full"
-        />
-        <Button href="#uphill" class="w-full">Cantilever</Button>
-      </div>
-    </div>
-  </Container>
-
-  <Container class="flex flex-col md:grid md:grid-cols-2 gap-6" id="uphill">
-    <img
-      src="/products/residential-gates/sliding/uphill_button.gif"
-      alt=""
-      class="object-contain object-center w-full h-full"
-    />
-    <div class="flex flex-col col-span-1">
-      <h3>How does uphill slide gate work</h3>
-      <ul class="ulink">
-        <li>The gate is trapezoidal to match the slope.</li>
-        <li>The entire weight of the gate rests on a rails.</li>
-        <li>
-          Powerd by Tousek frequency converter motor withc mechanical brake to
-          arrest the gate in stop position.
-        </li>
-        <li>
-          One-way reduction gear to counter the downward m•g•Sinθ component.
-        </li>
-      </ul>
-    </div>
-  </Container>
-  <Container class="flex flex-col md:grid md:grid-cols-2 gap-6" id="curved">
-    <img
-      src="/products/residential-gates/sliding/ssw_curved.gif"
-      alt=""
-      class="object-contain object-center w-full h-full"
-    />
-    <div class="flex flex-col col-span-1">
-      <h3>How does curved slide gate work</h3>
-      <ul class="ulink">
-        <li>
-          The entire weight of the gate rests on a curved rails with turning
-          radius of 0.5m.
-        </li>
-        <li>Each leaf is about 1m in width.</li>
-        <li>
-          Powerd by Tousek frequency converter motor for maximum torque at very
-          low starting speed.
-        </li>
-        <li>Wall-mount top track for stability.</li>
-      </ul>
-    </div>
-  </Container>
-  <Container class="flex flex-col md:grid md:grid-cols-2 gap-6" id="telescope">
-    <img
-      src="/products/residential-gates/sliding/TELESCOPIC.gif"
-      alt=""
-      class="object-contain object-center w-full h-full"
-    />
-    <div class="flex flex-col col-span-1">
-      <h3>How does telescopic gate work with only 1 motor</h3>
-      <ul class="ulink">
-        <li>
-          The entire weight of the gate rests on the rails where the wheels
-          moves on, therefore no hanging track needed.
-        </li>
-        <li>The active leaf is driven by the motor through the teeth racks.</li>
-        <li>
-          The passive leaf is driven by a chain-and-sprocket telescopic gear. It
-          will travel as double speed.
-        </li>
-        <li>
-          (1) motor; (2) rail; (3) travel limit; (4) top roller bracket; (5)
-          safe strike; (6) teeth rack; (7) chain-and-sprocket drive.
-        </li>
-      </ul>
-    </div>
-  </Container>
-  <Container class="flex flex-col md:grid md:grid-cols-2 gap-6" id="cantilever">
-    <img
-      src="/products/residential-gates/sliding/Cantilever_gate_1.gif"
-      alt=""
-      class="object-contain object-center w-full h-full"
-    />
-    <div class="flex flex-col col-span-1">
-      <h3>How does cantilever gate work without ground track</h3>
-      <ul class="ulink">
-        <li>
-          The entire weight of the gate rests on stationary carriages where the
-          gate slides on, therefore no track needed.
-        </li>
-        <li>
-          Automation of cantilever gate is the same as slide gate with track.
-        </li>
-        <li>
-          The gate opener only needs to overcome inertia and friction to move
-          the gate. Gate can go up to 10 meter in length and 500 Kg in weight.
-          Material can be stainless steel, wrought iron, brass, Aluminum or
-          glass.
-        </li>
-        <li>
-          (1) carriages; (2) parking wheel; (3) monorail; (4) guide-in fork; (5)
-          turnbuckle; (6) guide bracket; (Z) diagonal tie; (D) diagonal tie.
-        </li>
-      </ul>
-    </div>
-  </Container>
+    </Container>
+  {/each}
 </Container>
