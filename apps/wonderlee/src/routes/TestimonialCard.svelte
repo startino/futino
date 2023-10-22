33eacdbe953e82ff7926497ba169f38fa42bf2d7
Jorge Lewis <antopiahk@gmail.com>
2023-10-22 23:48:33 +0800

    testimonial section

---
+++ apps/wonderlee/src/routes/TestimonialCard.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { Card, Prose } from "lorc";
+  export let name: string = "no name provided";
+  export let body: string = "no body provided";
+  export let href: string = "no link";
+  export let img: string = "no img";
+</script>
+
+<Card
+  class="{$$props.class} bg-surface/40"
+  {href}
+  border="border"
+  size="max-w-none h-full w-full"
+>
+  <figure class="m-0 sm:m-0 flex flex-col gap-4 h-full not-prose">
+    <blockquote class="">
+      <Prose>
+        <p class="m-0 sm:m-0 text-surface-on tracking-tight leading-snug">
+          "{body}"
+        </p>
+      </Prose>
+    </blockquote>
+    <figcaption
+      class="flex flex-row gap-x-3 items-center justify-self-end mt-auto not-prose"
+    >
+      <img class="h-10 w-10 flex-none rounded-full" src={img} alt="" />
+      <Prose>
+        <h3 class="m-0 sm:m-0">{name}</h3>
+      </Prose>
+    </figcaption>
+  </figure>
+</Card>
--- packages/lorc/src/lib/index.ts
+++ packages/lorc/src/lib/index.ts
@@ -8,12 +8,14 @@ export { default as Container } from './components/atoms/Container.svelte';
 export { default as Card } from './components/atoms/Card.svelte';
 export { default as SelectMenu } from './components/atoms/SelectMenu.svelte';
 export { default as Authenticate } from './components/atoms/Authenticate.svelte';
+export { default as TestimonialCard } from './components/atoms/TestimonialCard.svelte';
 // Molecules
 export { default as FatFooter } from './components/molecules/FatFooter.svelte';
 export { default as Footer } from './components/molecules/Footer.svelte';
 export { default as Header } from './components/molecules/Header.svelte';
 export { default as Hero } from './components/molecules/Hero.svelte';
 export { default as ContactForm } from './components/molecules/ContactForm.svelte';
+export { default as Testimonials } from './components/molecules/Testimonials.svelte'
 // Organisms
 export { default as Shell } from './components/organisms/Shell.svelte';
 
