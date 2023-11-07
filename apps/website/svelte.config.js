8087bd6b1685096d81a0a26432fb19cc45ed2832
Jonas Lindberg <eksno@protonmail.com>
2023-11-07 14:17:04 +0100

    Change website adapter to vercel (#206)

--- apps/website/svelte.config.js
+++ apps/website/svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import adapter from 'svelte-adapter-bun';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { mdsvex } from 'mdsvex';
 
