4134fddefbd66319868455c947cbb1757d12a49e
Jonas Lindberg <eksno@protonmail.com>
2023-12-18 21:05:30 +0100

    Futino add Portfolio (#283)
    Co-authored-by: antopiahk <antopiahk@gmail.com>

--- apps/website/src/lib/components/atoms/Button.svelte
+++ apps/website/src/lib/components/atoms/Button.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
-	export let href: string = '';
+  export let href: string = "";
 </script>
 
 <a
-	{href}
-	on:click={$$props.onClick}
-	class="{$$props.class} flex relative border-2 border-primary hover:translate-y-1 justify-center m-2 tracking-wide items-center py-3 px-6 font-bold rounded-md group hover:text-primary-container-on hover:bg-primary-container/60 transition-all duration-300 hover:scale-[97%] {$$props.secondary
-		? ' text-primary bg-surface'
-		: 'text-primary-on bg-primary'}">
-	<span class="relative z-0 uppercase">
-		<slot />
-	</span></a>
+  {href}
+  on:click={$$props.onClick}
+  target={$$props.target}
+  class="{$$props.class} flex relative border-2 border-primary hover:translate-y-1 justify-center m-2 tracking-wide items-center py-3 px-6 font-bold rounded-md group hover:text-primary-container-on hover:bg-primary-container/60 transition-all duration-300 hover:scale-[97%] {$$props.secondary
+    ? ' text-primary bg-surface'
+    : 'text-primary-on bg-primary'}"
+>
+  <span class="relative z-0 uppercase">
+    <slot />
+  </span></a
+>
