3d8cddd6f43af856054d7c6ac8ee5f55adb17e8c
Jonas Lindberg <eksno@protonmail.com>
2024-01-07 21:56:13 +0100

    developing on sfk

--- apps/sfk/src/routes/+page.svelte
+++ apps/sfk/src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
 	import '../app.pcss';
+	import LoremIpsum from '$lib/components/LoremIpsum.svelte';
 </script>
 
 <!-- Hero? -->
 <section class="flex max-w-5xl flex-col items-center justify-center text-center">
-	<h1>Who we are</h1>
-	<p>Lorem ipsum</p>
+	<h1 class="text-4xl">Who we are</h1>
+	<LoremIpsum name="jeff" />
+	<LoremIpsum name="bill" />
 </section>
