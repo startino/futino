3fc862764d62dffa9e60ae3980474c7c781cc7e3
Jorge Lewis <31695089+antopiahk@users.noreply.github.com>
2024-01-13 19:46:34 +0800

    oak project init

---
+++ apps/oak/src/lib/components/molecules/LegalPage.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Prose from "../atoms/Prose.svelte";
+</script>
+
+<main
+  class="{$$props.class} py-40 px-6 border-b shadow-2xl sm:px-6 md:px-8 border-primary/40 prose-headings:font-sans"
+>
+  <Prose size="xs">
+    <article class="text-left max-w-7xl justify-items-center space-y-8 mx-auto">
+      <slot />
+    </article>
+  </Prose>
+</main>
---
+++ apps/oak/src/lib/components/organisms/FAQSection.svelte
@@ -0,0 +1,80 @@
+<script lang="ts">
+  import Button from "../atoms/Button.svelte";
+  import FAQCard from "../atoms/FAQCard.svelte";
+  import SearchBox from "../atoms/SearchBox.svelte";
+
+  // Gets the Frequently Asked Questions by parent component
+  export let faqs: {
+    question: string;
+    answer: string;
+  }[] = [];
+  let questionEntry: string;
+
+  const handleClick = (question: string) => () => (questionEntry = question);
+
+  // For Search Input
+  let searchInput = "";
+  let filteredFaqs: {
+    question: string;
+    answer: string;
+  }[] = [];
+
+  // Matches photoData
+  const searchPhotos = () => {
+    return (filteredFaqs = faqs.filter((faq) => {
+      if (faq.question.toLowerCase().includes(searchInput.toLowerCase()))
+        return true;
+      if (faq.answer.toLowerCase().includes(searchInput.toLowerCase()))
+        return true;
+    }));
+  };
+</script>
+
+<div class="grid grid-cols-1 md:grid-cols-5 gap-3 w-full">
+  <div
+    class="col-span-2 h-fit flex flex-col gap-y-4 items-center self-start md:items-start md:text-left text-surface-on"
+  >
+    <h1 class="font-semibold headline-medium md:display-medium leading-tight">
+      Your Questions, <br class="hidden md:flex" /> Answered
+    </h1>
+    <h6 class="title-medium hidden md:flex">
+      Find clarity and confidence <br /> through some frequently asked questions.
+      If you can't find your answer there, we're open to answer any questions.
+    </h6>
+    <div class="flex flex-row md:flex-col lg:flex-row gap-y-4 -ml-2 my-2 mb-6">
+      <Button href="/contact">
+        <h2 class="title-medium">Book a call</h2>
+      </Button>
+      <Button href="/contact">
+        <h2 class="title-medium">Contact</h2>
+      </Button>
+    </div>
+  </div>
+  <div class="flex flex-col gap-4 md:col-span-3 self-start w-full">
+    <div class="lex flex-col gap-4 items-start">
+      <SearchBox
+        class="w-full mb-4 md:mb-6"
+        bind:searchInput
+        on:input={searchPhotos}
+      />
+      <div class="flex flex-col gap-4 overflow-y-scroll w-full max-h-[505px]">
+        {#if searchInput && filteredFaqs.length === 0}
+          <div class="w-full py-12 headline-medium inline">
+            <h6 class="">Sorry, we couldn't find your question.</h6>
+            <a href="/contact" class="underline text-tertiary headline-small"
+              >Ask Us Here!</a
+            >
+          </div>
+        {:else if filteredFaqs.length > 0}
+          {#each filteredFaqs as { question, answer }}
+            <FAQCard {question} {answer} isOpen={question == questionEntry} />
+          {/each}
+        {:else}
+          {#each faqs as { question, answer }}
+            <FAQCard {question} {answer} isOpen={question == questionEntry} />
+          {/each}
+        {/if}
+      </div>
+    </div>
+  </div>
+</div>
