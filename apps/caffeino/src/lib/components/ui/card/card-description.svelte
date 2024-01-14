06704693886d42f923c8e9d6cb1ef54c094ae50e
Jonas Lindberg <eksno@protonmail.com>
2024-01-14 13:11:53 +0100

    Caffeino (#359)

---
+++ apps/caffeino/src/lib/components/ui/card/card-description.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { HTMLAttributes } from 'svelte/elements';
+	import { cn } from '$lib/utils';
+
+	type $$Props = HTMLAttributes<HTMLParagraphElement>;
+
+	let className: $$Props['class'] = undefined;
+	export { className as class };
+</script>
+
+<p class={cn('text-surface-on max-w-xl', className)} {...$$restProps}>
+	<slot />
+</p>
---
+++ apps/caffeino/src/lib/components/ui/section/index.ts
@@ -0,0 +1,6 @@
+import Section from "./section.svelte";
+
+export {
+  Section as Section
+	};
+
