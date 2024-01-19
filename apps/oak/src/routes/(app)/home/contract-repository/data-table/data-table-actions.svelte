f4117fc1d341e6876fc4d8e93b5b6ce2d44b7b1c
Nazif Barassounon <57366738+nazifbara@users.noreply.github.com>
2024-01-19 18:36:28 +0100

    Add the contract repo data table

---
+++ apps/oak/src/routes/(app)/home/contract-repository/data-table/data-table-actions.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
+	import { Button } from '$lib/components/ui/button';
+	import { DotsHorizontal } from 'radix-icons-svelte';
+
+	export let id: string;
+</script>
+
+<DropdownMenu.Root>
+	<DropdownMenu.Trigger asChild let:builder>
+		<Button variant="ghost" builders={[builder]} class="relative h-8 w-8 p-0">
+			<span class="sr-only">Open menu</span>
+			<DotsHorizontal class="h-4 w-4" />
+		</Button>
+	</DropdownMenu.Trigger>
+	<DropdownMenu.Content>
+		<DropdownMenu.Group>
+			<DropdownMenu.Label>Actions</DropdownMenu.Label>
+			<DropdownMenu.Item on:click={() => navigator.clipboard.writeText(id)}>
+				Copy payment ID
+			</DropdownMenu.Item>
+		</DropdownMenu.Group>
+		<DropdownMenu.Separator />
+		<DropdownMenu.Item>View customer</DropdownMenu.Item>
+		<DropdownMenu.Item>View payment details</DropdownMenu.Item>
+	</DropdownMenu.Content>
+</DropdownMenu.Root>
