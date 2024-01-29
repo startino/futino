ad7540c06e9e246a14d884f6f358f658078c71dd
Nazif Barassounon <57366738+nazifbara@users.noreply.github.com>
2024-01-29 11:32:31 +0100

    fix futino's portfolio section bug (#435)

--- apps/website/src/routes/ClientCard.svelte
+++ apps/website/src/routes/ClientCard.svelte
@@ -1,48 +1,43 @@
 <script>
-  import Button from "$lib/components/atoms/Button.svelte";
+	import Button from '$lib/components/atoms/Button.svelte';
 </script>
 
 <div
-  class="{$$props.class} group bg-surface rounded-md items-center max-w-lg md:max-w-7xl w-full lg:items-start grid grid-cols-1 md:grid-cols-2 gap-4 py-4 px-4 md:px-8 md:py-10 lg:px-10 drop-shadow-glow-sm border-1 border-primary"
+	class="{$$props.class} group grid w-full max-w-lg grid-cols-1 items-center gap-4 rounded-md border-1 border-primary bg-surface px-4 py-4 drop-shadow-glow-sm md:max-w-7xl md:grid-cols-2 md:px-8 md:py-10 lg:items-start lg:px-10"
 >
-  <div class="flex flex-col h-full gap-2 lg:text-left z-10 text-left">
-    <h1 class="display-medium">{$$props.name}</h1>
-    <h3 class=" max-w-sm">
-      {$$props.body}
-    </h3>
-    <ul
-      class="flex flex-row flex-wrap headline-medium items-center py-4 gap-1 place-items-center"
-    >
-      <li class="title-medium">Services:</li>
-      <br class="w-96 sm:hidden" />
-      {#each $$props.services as service, i}
-        <li class="title-medium font-bold my-auto">
-          {service}
-          {#if i !== $$props.services.length - 1},{/if}
-        </li>
-      {/each}
-    </ul>
-    <Button
-      secondary
-      class="justify-self-end mt-auto"
-      href={$$props.href}
-      target="_blank">Check it out!</Button
-    >
-  </div>
-  <a
-    href={$$props.href}
-    class="flex flex-row flex-nowrap {$$props.bgColor} h-full w-full mx-auto z-0 rounded-xl not-prose p-2 py-6 sm:p-6 md:p-6 justify-center gap-2 md:gap-3 items-center"
-  >
-    <img
-      class="w-[230px] sm:w-[350px] md:w-[250px] lg:w-[300px] xl:w-[400px] 2xl:w-[425px] basis-2 shrink object-contain not-prose"
-      src={$$props.desktop_src}
-      alt="{$$props.company} desktop mockup"
-    />
+	<div class="z-10 flex h-full flex-col gap-2 text-left lg:text-left">
+		<h1 class="display-medium">{$$props.name}</h1>
+		<h3 class=" max-w-sm">
+			{$$props.body}
+		</h3>
+		<ul class="headline-medium flex flex-row flex-wrap place-items-center items-center gap-1 py-4">
+			<li class="title-medium">Services:</li>
+			<br class="w-96 sm:hidden" />
+			{#each $$props.services as service, i}
+				<li class="title-medium my-auto font-bold">
+					{service}
+					{#if i !== $$props.services.length - 1},{/if}
+				</li>
+			{/each}
+		</ul>
+		<Button secondary class="mt-auto justify-self-end" href={$$props.href} target="_blank"
+			>Check it out!</Button
+		>
+	</div>
+	<a
+		href={$$props.href}
+		class="flex flex-row flex-nowrap {$$props.bgColor} not-prose z-0 mx-auto h-full w-full items-center justify-center gap-2 rounded-xl p-2 py-6 sm:p-6 md:gap-3 md:p-6"
+	>
+		<img
+			class="not-prose w-[230px] shrink basis-2 object-contain sm:w-[350px] md:w-[250px] lg:w-[300px] xl:w-[400px] 2xl:w-[425px]"
+			src={$$props.desktop_src}
+			alt="{$$props.company} desktop mockup"
+		/>
 
-    <img
-      class="w-[53px] sm:w-[100px] md:w-[60px] lg:w-[75px] xl:w-[100px] h-fit object-contain not-prose"
-      src={$$props.phone_src}
-      alt="{$$props.company} phone mockup"
-    />
-  </a>
+		<img
+			class="not-prose w-[53px] object-contain sm:w-[100px] md:w-[60px] lg:w-[75px] xl:w-[100px]"
+			src={$$props.phone_src}
+			alt="{$$props.company} phone mockup"
+		/>
+	</a>
 </div>
