d1ee0128f268c4e267bee13a9ac9310db5711bc2
Nazif Barassounon <57366738+nazifbara@users.noreply.github.com>
2024-01-30 18:43:31 +0100

    display subscription renewal date

--- apps/oak/src/routes/app/subscription/+page.svelte
+++ apps/oak/src/routes/app/subscription/+page.svelte
@@ -1,17 +1,30 @@
-<script>
+<script lang="ts">
 	import * as Card from '$lib/components/ui/card';
 	import { Button } from '$lib/components/ui/button';
 	import Pricing from './pricing.svelte';
+	import type { PageData } from './$types';
 
-	export let data;
+	export let data: PageData;
+
+	const renewalDate = new Date(data.subscription.current_period_end * 1000).toLocaleDateString(
+		undefined,
+		{
+			year: 'numeric',
+			month: 'long',
+			day: '2-digit'
+		}
+	);
 </script>
 
 <Card.Root class="p-10">
 	<Card.Header><Card.Title class="m-0 sm:m-0">Your Subscription</Card.Title></Card.Header>
 	<Card.Content>
 		{#if data.subscription.status === 'active'}
-			<p class="mb-10 text-primary-500">
+			<p class="mb-4 text-primary-500">
 				Your {data.subscription.is_monthly ? 'monthly' : 'yearly'} subscription is active
+			</p>
+			<p>
+				Renewal Date: {renewalDate}
 			</p>
 		{:else}
 			<Pricing />
