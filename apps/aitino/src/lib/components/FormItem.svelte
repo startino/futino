3c6dde2132c4b80f4f4b6e6f79ffb9ba9888628b
Minilik Zeru <95419283+Minilik94@users.noreply.github.com>
2024-02-15 17:20:05 +0300

    Changing icons, and fixing the sonner delay (#521)

--- apps/aitino/src/lib/components/FormItem.svelte
+++ apps/aitino/src/lib/components/FormItem.svelte
@@ -2,19 +2,15 @@
 	import { applyAction, enhance } from '$app/forms';
 	import { invalidateAll } from '$app/navigation';
 	import { Button } from '$lib/components/ui/button';
-	import { Input } from '$lib/components/ui/input';
-	// import { toast } from 'svelte-sonner'
-</script>
 
-<svelte:head>
-	<link
-		rel="stylesheet"
-		href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-		integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
-		crossorigin="anonymous"
-		referrerpolicy="no-referrer"
-	/>
-</svelte:head>
+	import { ArrowRight, Loader, Twitter, X, Youtube } from 'lucide-svelte';
+	import Input from './ui/input/input.svelte';
+	import { toast } from 'svelte-sonner';
+
+	export let form: any = {};
+
+	let isLoading = false;
+</script>
 
 <form
 	action="?/register"
@@ -28,8 +24,8 @@
 	}}
 >
 	<div class="flex w-full flex-col p-1">
-		<input
-			class="border-input placeholder:text-muted-foreground focus-visible:ring-ring flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50"
+		<Input
+			class="border-input placeholder:text-muted-foreground focus-visible:ring-ring flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-sm shadow-sm ring-offset-0 transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 "
 			id="email"
 			type="email"
 			name="email"
@@ -40,9 +36,17 @@
 		<!-- svelte-ignore missing-declaration -->
 		<Button
 			type="submit"
-			class="hover:bg-primary relative -ml-1 flex w-full gap-8 text-lg transition-all duration-300 ease-in-out"
-			>Join the waitlist <i class="fas fa-arrow-right absolute right-8"></i></Button
+			class="relative -ml-1 flex w-full gap-8 text-lg transition-all duration-300 ease-in-out"
+			on:click={async () => {
+				if (form?.success) {
+					toast.success(form.message);
+				} else {
+					toast.error(form?.error);
+				}
+			}}
 		>
+			Join the waitlist<ArrowRight />
+		</Button>
 	</div>
 
 	<div class="mt-12 flex w-full items-center justify-between md:p-4">
@@ -51,7 +55,7 @@
 				class="hover:bg-primary bg-transparent p-8 font-bold transition-all duration-300 ease-in-out hover:scale-95 hover:text-[#17151c]"
 				href="https://x.com/@futino"
 			>
-				<i class="fa-brands fa-twitter pr-2 text-2xl"></i>
+				<Twitter size="28" />
 				<div class="flex flex-col">
 					<p class="px-4 py-4 text-base">
 						Twitter
@@ -66,7 +70,7 @@
 				class="hover:bg-primary bg-transparent p-8 font-bold transition-all duration-300 ease-in-out hover:scale-95 hover:text-[#17151c]"
 				href="https://youtube.com/@futino"
 			>
-				<i class="fa-brands fa-youtube text-xl"></i>
+				<Youtube size="28" />
 				<div class="flex flex-col">
 					<p class="px-4 py-4 text-base">
 						Youtube
