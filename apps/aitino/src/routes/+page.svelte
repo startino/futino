907073ba6a7de23cf55ce693d769bf78477875b9
Jorge Lewis <31695089+antopiahk@users.noreply.github.com>
2024-02-16 00:34:00 +0800

    some ui changes

--- apps/aitino/src/routes/+page.svelte
+++ apps/aitino/src/routes/+page.svelte
@@ -15,54 +15,17 @@
 </script>
 
 <div class="flex h-full flex-col justify-evenly py-6">
-	<Dialog.Root>
-		<Dialog.Trigger class="ml-auto"
-			><Button class="ml-auto block max-w-sm  text-base">Contact Us</Button></Dialog.Trigger
-		>
-		<Dialog.Content class="sm:max-w-[425px]">
-			<Dialog.Header class="mt-4 space-y-1">
-				<Dialog.Title class="text-4xl">Contact Us</Dialog.Title>
-			</Dialog.Header>
-			<div class="grid gap-2">
-				<Label for="name">Name</Label>
-				<Input
-					class="border-input placeholder:text-muted-foreground focus-visible:ring-ring flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-sm shadow-sm ring-offset-0 transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 "
-					id="name"
-					type="name"
-					name="name"
-				/>
-			</div>
-			<div class="grid gap-2">
-				<Label for="email">Email</Label>
-				<Input
-					class="border-input placeholder:text-muted-foreground focus-visible:ring-ring flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-sm shadow-sm ring-offset-0 transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 "
-					id="email"
-					type="email"
-					name="email"
-				/>
-			</div>
-			<div class="grid gap-2">
-				<Label for="email">Description</Label>
-				<Textarea
-					class="border-input placeholder:text-muted-foreground focus-visible:ring-ring flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-sm shadow-sm ring-offset-0 transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 "
-				/>
-			</div>
-			<Dialog.Footer>
-				<Button type="submit">Submit</Button>
-			</Dialog.Footer>
-		</Dialog.Content>
-	</Dialog.Root>
 
-	<div class="relative mx-auto flex w-full flex-col gap-y-12 text-white lg:gap-y-4">
-		<h1 class="text-center text-2xl md:text-4xl lg:text-6xl xl:text-7xl">
+	<div class="relative mx-auto flex flex-col place-items-center gap-y-4 md:gap-y-12 text-foreground lg:gap-y-4">
+		<h1 class="text-center text-2xl md:text-4xl lg:text-6xl xl:text-7xl m-0 sm:m-0">
 			Join the Future of AI<br /> with Exclusive Early Access to
 			<span class="text-primary border-none">Aitino</span>
 		</h1>
 
-		<p class="mx-auto mt-2 max-w-3xl text-center text-lg">
-			<span class="text-primary">Aitino </span> is a SaaS platform powered by multi-agent LLMs designed
+		<h2 class="mx-auto mt-2 text-center prose text-foreground m-0 sm:m-0">
+			<span class="text-primary">Aitino </span> is a platform powered by multi-agent LLMs designed
 			to help entrepreneurs and businesses automate tasks and solve complex problems.
-		</p>
+		</h2>
 
 		<FormItem class="mt-10" {form} />
 	</div>
