14c7ffad5624d9d5dfcfcdb226a42ad9d19558e6
Jorge Lewis <31695089+antopiahk@users.noreply.github.com>
2024-02-15 17:44:03 +0800

    Merging aitino waitlist branch into alpha-aitino (#514)
    Signed-off-by: Jorge Lewis <31695089+antopiahk@users.noreply.github.com>
    Co-authored-by: Nazif Barassounon <57366738+nazifbara@users.noreply.github.com>
    Co-authored-by: Minilik94 <minilikzeru@gmail.com>

---
+++ apps/aitino/src/lib/components/Contact.svelte
@@ -0,0 +1,65 @@
+<script>
+	import { Button } from '$lib/components/ui/button';
+	import { Input } from '$lib/components/ui/input';
+	import * as Card from '$lib/components/ui/card';
+	import { Label } from '$lib/components/ui/label';
+	import Textarea from '$lib/components/ui/textarea/textarea.svelte';
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+	export function closeModal() {
+		dispatch('closeModal');
+	}
+
+	export let showModal = false;
+
+	$: showModal;
+</script>
+
+<Card.Root
+	class="bg-card border-primary absolute left-1/2 top-1/2  w-full max-w-xl -translate-x-1/2  -translate-y-1/2 border font-bold text-primary transition-all duration-300 ease-in-out"
+>
+	<Button
+		class="absolute right-0 rounded-lg bg-transparent hover:scale-95 hover:bg-transparent"
+		on:click={() => closeModal()}><i class="fas fa-times"></i></Button
+	>
+	<Card.Header class="space-y-1 mt-4">
+		<Card.Title class="text-4xl">Contact Us</Card.Title>
+	</Card.Header>
+	<Card.Content class="grid gap-4">
+		<div class="grid gap-2">
+			<Label for="name">Name</Label>
+			<Input
+				id="name"
+				type="text"
+				placeholder="John Doe"
+				class="font-roboto w-full border-none bg-[#17151c] p-6 font-bold  outline-primary focus-within:outline-primary focus:outline"
+			/>
+		</div>
+		<div class="grid gap-2">
+			<Label for="email">Email</Label>
+			<Input
+				id="email"
+				type="email"
+				placeholder="example@example.com"
+				class="font-roboto w-full border-none bg-[#17151c] p-6 font-bold  outline-primary focus-within:outline-primary focus:outline"
+			/>
+		</div>
+		<div class="grid gap-2">
+			<Label for="email">Description</Label>
+			<Textarea
+				id="description"
+				placeholder="description..."
+				class="font-roboto w-full border-none bg-[#17151c] p-6 font-bold  outline-primary focus-within:outline-primary focus:outline"
+			/>
+		</div>
+	</Card.Content>
+	<Card.Footer>
+		<Button
+			class="relative w-full border-none bg-primary text-lg font-bold text-black transition-all duration-300 ease-in-out hover:scale-y-95 hover:bg-[#003825]"
+			on:click={() => (showModal = false)}
+			type="submit">Submit</Button
+		>
+	</Card.Footer>
+</Card.Root>
---
+++ apps/aitino/src/lib/components/FormItem.svelte
@@ -0,0 +1,80 @@
+<script lang="ts">
+	import { applyAction, enhance } from '$app/forms';
+	import { invalidateAll } from '$app/navigation';
+	import { Button } from '$lib/components/ui/button';
+	import { Input } from '$lib/components/ui/input';
+	// import { toast } from 'svelte-sonner'
+</script>
+
+<svelte:head>
+	<link
+		rel="stylesheet"
+		href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+		integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+		crossorigin="anonymous"
+		referrerpolicy="no-referrer"
+	/>
+</svelte:head>
+
+<form
+	action="?/register"
+	method="POST"
+	class="mx-auto flex w-full max-w-sm flex-col items-center justify-center space-x-2 lg:max-w-lg {$$props.class}"
+	use:enhance={() => {
+		return async ({ result }) => {
+			invalidateAll();
+			await applyAction(result);
+		};
+	}}
+>
+	<div class="flex w-full flex-col p-1">
+		<input
+			class="border-input placeholder:text-muted-foreground focus-visible:ring-ring flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50"
+			id="email"
+			type="email"
+			name="email"
+			placeholder="jorge.lewis@futi.no"
+		/>
+	</div>
+	<div class="w-full border-none p-1">
+		<!-- svelte-ignore missing-declaration -->
+		<Button
+			type="submit"
+			class="hover:bg-primary relative -ml-1 flex w-full gap-8 text-lg transition-all duration-300 ease-in-out"
+			>Join the waitlist <i class="fas fa-arrow-right absolute right-8"></i></Button
+		>
+	</div>
+
+	<div class="mt-12 flex w-full items-center justify-between md:p-4">
+		<div class="flex">
+			<Button
+				class="hover:bg-primary bg-transparent p-8 font-bold transition-all duration-300 ease-in-out hover:scale-95 hover:text-[#17151c]"
+				href="https://x.com/@futino"
+			>
+				<i class="fa-brands fa-twitter pr-2 text-2xl"></i>
+				<div class="flex flex-col">
+					<p class="px-4 py-4 text-base">
+						Twitter
+						<br />
+						@futino
+					</p>
+				</div>
+			</Button>
+		</div>
+		<div class="flex">
+			<Button
+				class="hover:bg-primary bg-transparent p-8 font-bold transition-all duration-300 ease-in-out hover:scale-95 hover:text-[#17151c]"
+				href="https://youtube.com/@futino"
+			>
+				<i class="fa-brands fa-youtube text-xl"></i>
+				<div class="flex flex-col">
+					<p class="px-4 py-4 text-base">
+						Youtube
+						<br />
+						@futino
+					</p>
+				</div>
+			</Button>
+		</div>
+	</div>
+</form>
