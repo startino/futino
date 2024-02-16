c93f18026547ff42ef8c0fb8211af146ca9e4132
Jorge Lewis <31695089+antopiahk@users.noreply.github.com>
2024-02-16 18:36:47 +0800

    removing custom splittest component since using package's

--- apps/aitino/src/lib/components/split-test/index.ts
+++
@@ -1,4 +0,0 @@
-import SplitTest from "./SplitTest.svelte"
-import { serverGetSplitTestIdentifier, clientGetSplitTestIdentifier, getVariant, performSplitTestAction } from "./splitTesting.js"
-
-export { SplitTest, serverGetSplitTestIdentifier, clientGetSplitTestIdentifier, getVariant, performSplitTestAction }
