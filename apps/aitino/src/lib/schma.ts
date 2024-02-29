bee2dcb2d63d585becfa9cac4b7f3a44fe4057e9
Minilik94 <minilzeru@gmail.com>
2024-02-29 16:29:56 +0300

    fixed issue with validation in the schema

--- apps/aitino/src/lib/schma.ts
+++ apps/aitino/src/lib/schma.ts
@@ -8,14 +8,11 @@ export const formSchema = z.object({
 	email: z.string().email({ message: "Invalid email address" }),
 	password: z
 		.string()
-		.min(8, { message: "Password must be at least 8 characters long.\n" })
-		.max(100, { message: "Password must be 100 characters or less.\n" })
-		.regex(/[a-z][A-Z][0-9]/, {
-			message:
-				"Password must contain at least one lowercase letter.\n Password must contain at least one uppercase letter.\n Password must contain at least one number.\n"
-		})
-	//, { message: "Password must contain at least one uppercase letter.\n" })
-	//, { message: "Password must contain at least one number.\n" })
+		.min(8, { message: "Password must be at least 8 characters long." })
+		.max(100, { message: "Password must be 100 characters or less." })
+		.regex(/[a-z]/, { message: "Password must contain at least one lowercase letter." })
+		.regex(/[A-Z]/, { message: "Password must contain at least one uppercase letter." })
+		.regex(/[0-9]/, { message: "Password must contain at least one number." })
 });
 
 export type FormSchema = typeof formSchema;
