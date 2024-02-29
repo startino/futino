36d5ea6057479fddf8521b046243b23b6d14ed8a
Nazif Barassounon <57366738+nazifbara@users.noreply.github.com>
2024-02-29 08:56:03 +0100

    fix the getUserBillingMethod bad import

--- apps/oak/src/routes/app/subscription/+layout.server.ts
+++ apps/oak/src/routes/app/subscription/+layout.server.ts
@@ -1,8 +1,5 @@
-import { getUserBillingMethod } from '$lib/api-client.js';
-
-export const load = async ({ locals: { stripe, supabase, getSession } }) => {
-	const { user } = await getSession();
-	const paymentMethod = await getUserBillingMethod({ stripe, supabase, user });
+export const load = async ({ locals: { apiClient } }) => {
+	const paymentMethod = await apiClient.getUserBillingMethod();
 	return {
 		paymentMethod: paymentMethod ? { card: paymentMethod.card, id: paymentMethod.id } : null
 	};
