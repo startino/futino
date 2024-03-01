bab868364314f1cac2fd68e86304a1c244f45390
Jonas Lindberg <eksno@protonmail.com>
2024-03-01 06:30:13 +0100

    safer CORS

--- apps/aitino/src/hooks.server.ts
+++ apps/aitino/src/hooks.server.ts
@@ -25,18 +25,9 @@ export const handle: Handle = async ({ event, resolve }) => {
 		return session;
 	};
 
-	// Required for CORS to work
-	if (event.request.method === "OPTIONS") {
-		return new Response(null, {
-			headers: {
-				"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-				"Access-Control-Allow-Origin": "*",
-				"Access-Control-Allow-Headers": "*"
-			}
-		});
-	}
-
-	const response = await resolve(event);
-	response.headers.append("Access-Control-Allow-Origin", `*`);
-	return response;
+	return await resolve(event, {
+		filterSerializedResponseHeaders(name) {
+			return name === "content-range";
+		}
+	});
 };
