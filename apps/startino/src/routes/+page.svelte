7657b4841415f5be23f693792ae2872a3a1689d2
Jonas Lindberg <eksno@protonmail.com>
2024-03-16 12:08:13 +0100

    startino finished apart from photos

--- apps/startino/src/routes/+page.svelte
+++ apps/startino/src/routes/+page.svelte
@@ -31,14 +31,15 @@
 		Many non-tech founders struggle to build out their software idea in a holistic manner; retiring
 		before they finish their MVP or giving half their business to someone untrustworthy.
 	</p>
+	<!-- TODO: Make these buttons functional and uncomment them -->
 	<div class="flex gap-4">
 		<button
-			class="inline-flex h-10 items-center justify-center whitespace-nowrap rounded bg-primary p-8 text-xl font-bold text-primary-on ring-offset-background transition-colors hover:scale-[98%] hover:bg-accent hover:text-accent-on focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+			class="inline-flex h-10 items-center justify-center whitespace-nowrap rounded bg-primary p-8 text-xl font-bold text-primary-on ring-offset-background transition-colors hover:scale-[98%] hover:bg-tertiary hover:text-tertiary-on focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
 		>
 			Contact Us
 		</button>
 		<button
-			class="inline-flex h-10 items-center justify-center whitespace-nowrap rounded border border-input bg-background p-8 text-xl font-medium ring-offset-background transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+			class="inline-flex h-10 items-center justify-center whitespace-nowrap rounded border border-input bg-transparent p-8 text-xl font-medium text-primary ring-offset-background transition-colors hover:scale-[98%] hover:border-tertiary hover:text-tertiary focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
 		>
 			Book a Meeting
 		</button>
@@ -150,39 +151,41 @@
 			<li class="flex flex-col gap-6 xl:flex-row">
 				<img
 					class="aspect-[4/5] w-52 flex-none rounded-2xl bg-primary/10 object-cover"
-					src="https://futi.no/favicon.png"
+					src="/portraits/jorge-lewis.jpg"
 					alt=""
 				/>
 				<div class="flex-auto">
 					<h3 class="trackng-tight text-lg font-semibold leading-8 text-primary">Jorge Lewis</h3>
 					<p class="text-base leading-7 text-primary">Co-Founder</p>
 					<p class="mt-6 text-base leading-7 opacity-80">
-						Quia illum aut in beatae. Possimus dolores aliquid accusantium aut in ut non assumenda.
-						Enim iusto molestias aut deleniti eos aliquid magnam molestiae. At et non possimus ab.
-						Magni labore molestiae nulla qui.
+						Hey 👋. I've got over 6 years of programming experience under my belt, with an expertise
+						in automation software and machine learning. I've found a passion in building both
+						beautiful and highly functional software to bring life to projects like your own through
+						websites and custom software.
 					</p>
 				</div>
 			</li>
 			<li class="flex flex-col gap-6 xl:flex-row">
 				<img
 					class="aspect-[4/5] w-52 flex-none rounded-2xl bg-primary/10 object-cover"
-					src="https://futi.no/favicon.png"
+					src="/portraits/jonas-lindberg.jpg"
 					alt=""
 				/>
 				<div class="flex-auto">
 					<h3 class="trackng-tight text-lg font-semibold leading-8 text-primary">Jonas Lindberg</h3>
 					<p class="text-base leading-7 text-primary">Co-Founder</p>
 					<p class="mt-6 text-base leading-7 opacity-80">
-						Quia illum aut in beatae. Possimus dolores aliquid accusantium aut in ut non assumenda.
-						Enim iusto molestias aut deleniti eos aliquid magnam molestiae. At et non possimus ab.
-						Magni labore molestiae nulla qui.
+						Since the age of 14, my passion for coding has been a driving force in my life. I've
+						been deeply engaged in the creation of websites and bespoke software solutions in
+						collaboration with startups. Eager to dive into emerging tech, I'm all about driving
+						innovation and sharing the excitement!
 					</p>
 				</div>
 			</li>
 			<li class="flex flex-col gap-6 xl:flex-row">
 				<img
 					class="aspect-[4/5] w-52 flex-none rounded-2xl bg-primary/10 object-cover"
-					src="https://futi.no/favicon.png"
+					src="/portraits/nazif-barassounon.jpg"
 					alt=""
 				/>
 				<div class="flex-auto">
@@ -191,9 +194,10 @@
 					</h3>
 					<p class="text-base leading-7 text-primary">Web Developer</p>
 					<p class="mt-6 text-base leading-7 opacity-80">
-						Quia illum aut in beatae. Possimus dolores aliquid accusantium aut in ut non assumenda.
-						Enim iusto molestias aut deleniti eos aliquid magnam molestiae. At et non possimus ab.
-						Magni labore molestiae nulla qui.
+						I'm a full-stack developer at Futino with over 3 years of experience. I'm passionate
+						about creating web applications that blend technology and design seamlessly to deliver
+						user-friendly interfaces. I'm dedicated to learning and growing, always staying updated
+						with the latest web development trends.
 					</p>
 				</div>
 			</li>
@@ -206,27 +210,19 @@
 				<div class="flex-auto">
 					<h3 class="trackng-tight text-lg font-semibold leading-8 text-primary">Minilik Zeru</h3>
 					<p class="text-base leading-7 text-primary">Web Developer</p>
-					<p class="mt-6 text-base leading-7 opacity-80">
-						Quia illum aut in beatae. Possimus dolores aliquid accusantium aut in ut non assumenda.
-						Enim iusto molestias aut deleniti eos aliquid magnam molestiae. At et non possimus ab.
-						Magni labore molestiae nulla qui.
-					</p>
+					<p class="mt-6 text-base leading-7 opacity-80">Coming Soon...</p>
 				</div>
 			</li>
 			<li class="flex flex-col gap-6 xl:flex-row">
 				<img
 					class="aspect-[4/5] w-52 flex-none rounded-2xl bg-primary/10 object-cover"
-					src="https://futi.no/favicon.png"
+					src="/portraits/leon-nilsson.jpg"
 					alt=""
 				/>
 				<div class="flex-auto">
 					<h3 class="trackng-tight text-lg font-semibold leading-8 text-primary">Leon Nilsson</h3>
 					<p class="text-base leading-7 text-primary">Backend Developer</p>
-					<p class="mt-6 text-base leading-7 opacity-80">
-						Quia illum aut in beatae. Possimus dolores aliquid accusantium aut in ut non assumenda.
-						Enim iusto molestias aut deleniti eos aliquid magnam molestiae. At et non possimus ab.
-						Magni labore molestiae nulla qui.
-					</p>
+					<p class="mt-6 text-base leading-7 opacity-80">Coming Soon...</p>
 				</div>
 			</li>
 			<li class="flex flex-col gap-6 xl:flex-row">
@@ -238,13 +234,163 @@
 				<div class="flex-auto">
 					<h3 class="trackng-tight lading-8 text-lg font-semibold text-primary">Joshua Heath</h3>
 					<p class="text-base leading-7 text-primary">Sales</p>
-					<p class="mt-6 text-base leading-7 opacity-80">
-						Quia illum aut in beatae. Possimus dolores aliquid accusantium aut in ut non assumenda.
-						Enim iusto molestias aut deleniti eos aliquid magnam molestiae. At et non possimus ab.
-						Magni labore molestiae nulla qui.
-					</p>
+					<p class="mt-6 text-base leading-7 opacity-80">Coming Soon...</p>
 				</div>
 			</li>
 		</ul>
 	</div>
+</section>
+
+<section class="mx-auto flex max-w-7xl flex-col gap-8 px-6 pb-20 lg:px-8">
+	<div
+		class="md:prose-md prose prose-sm prose-main mx-auto flex flex-col items-center justify-center px-5 sm:prose-base lg:prose-lg xl:prose-xl 2xl:prose-2xl sm:px-10"
+	>
+		<h2 class="bg-gradient-to-b from-primary from-50% to-background bg-clip-text text-transparent">
+			<span class="opacity-30">Y</span>our Secret Sauce
+		</h2>
+		<p class="mt-6 text-pretty text-lg leading-8 opacity-80">
+			The Robust and Agile Tech Stack Empowering Innovation, Efficiency, and Scalability in Web
+			Development
+		</p>
+	</div>
+	<ul role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+		<li class="col-span-1 rounded-xl border border-primary shadow">
+			<a href="https://svelte.dev/" class="flex items-center justify-between space-x-6 p-6">
+				<div class="flex-1">
+					<div class="flex items-center space-x-3">
+						<h3 class="text-sm font-medium text-background-on">Svelte & SvelteKit</h3>
+						<span
+							class="inline-flex flex-shrink-0 items-center rounded-full bg-secondary px-1.5 py-0.5 text-xs font-medium text-secondary-on ring-1 ring-inset ring-green-600/20"
+							>Web Framework</span
+						>
+					</div>
+					<p class="mt-1 w-full text-pretty text-left text-sm text-background-on/80">
+						A web framework that shifts work out of the browser and into the build step, resulting
+						in faster and more efficient apps.
+					</p>
+				</div>
+				<img
+					class="h-10"
+					src="https://external-content.duckduckgo.com/ip3/svelte.dev.ico"
+					alt="Svelte Logo"
+				/>
+			</a>
+		</li>
+		<li class="col-span-1 rounded-xl border border-primary shadow">
+			<a
+				href="https://www.typescriptlang.org/"
+				class="flex items-center justify-between space-x-6 p-6"
+			>
+				<div class="flex-1">
+					<div class="flex items-center space-x-3">
+						<h3 class="text-sm font-medium text-background-on">TypeScript</h3>
+						<span
+							class="inline-flex flex-shrink-0 items-center rounded-full bg-secondary px-1.5 py-0.5 text-xs font-medium text-secondary-on ring-1 ring-inset ring-green-600/20"
+							>Language</span
+						>
+					</div>
+					<p class="mt-1 w-full text-pretty text-left text-sm text-background-on/80">
+						TypeScript is a strongly typed programming language that builds on JavaScript, giving
+						you better tooling at any scale.
+					</p>
+				</div>
+				<img
+					class="h-10"
+					src="https://external-content.duckduckgo.com/ip3/www.typescriptlang.org.ico"
+					alt="TypeScript Logo"
+				/>
+			</a>
+		</li>
+		<li class="col-span-1 rounded-xl border border-primary shadow">
+			<a href="https://vercel.com" class="flex items-center justify-between space-x-6 p-6">
+				<div class="flex-1">
+					<div class="flex items-center space-x-3">
+						<h3 class="text-sm font-medium text-background-on">Vercel</h3>
+						<span
+							class="inline-flex flex-shrink-0 items-center rounded-full bg-secondary px-1.5 py-0.5 text-xs font-medium text-secondary-on ring-1 ring-inset ring-green-600/20"
+							>Host</span
+						>
+					</div>
+					<p class="mt-1 w-full text-pretty text-left text-sm text-background-on/80">
+						Vercel is a platform that helps developers build, scale, and secure fast and
+						personalized web products.
+					</p>
+				</div>
+				<img
+					class="h-10"
+					src="https://external-content.duckduckgo.com/ip3/vercel.com.ico"
+					alt="Vercel Logo"
+				/>
+			</a>
+		</li>
+		<li class="col-span-1 rounded-xl border border-primary shadow">
+			<a href="https://www.cloudflare.com/" class="flex items-center justify-between space-x-6 p-6">
+				<div class="flex-1">
+					<div class="flex items-center space-x-3">
+						<h3 class="text-sm font-medium text-background-on">Cloudflare</h3>
+						<span
+							class="inline-flex flex-shrink-0 items-center rounded-full bg-secondary px-1.5 py-0.5 text-xs font-medium text-secondary-on ring-1 ring-inset ring-green-600/20"
+							>Networking</span
+						>
+					</div>
+					<p class="mt-1 w-full text-pretty text-left text-sm text-background-on/80">
+						Cloudflare is a global cloud platform that provides network services to protect and
+						optimize websites.
+					</p>
+				</div>
+				<img
+					class="h-10"
+					src="https://external-content.duckduckgo.com/ip3/www.cloudflare.com.ico"
+					alt="Cloudflare Logo"
+				/>
+			</a>
+		</li>
+		<li class="col-span-1 rounded-xl border border-primary shadow">
+			<a
+				href="https://fastapi.tiangolo.com/"
+				class="flex items-center justify-between space-x-6 p-6"
+			>
+				<div class="flex-1">
+					<div class="flex items-center space-x-3">
+						<h3 class="text-sm font-medium text-background-on">FastAPI</h3>
+						<span
+							class="inline-flex flex-shrink-0 items-center rounded-full bg-secondary px-1.5 py-0.5 text-xs font-medium text-secondary-on ring-1 ring-inset ring-green-600/20"
+							>API Framework</span
+						>
+					</div>
+					<p class="mt-1 w-full text-pretty text-left text-sm text-background-on/80">
+						FastAPI is a modern, fast (high-performance), web framework for building APIs with
+						Python.
+					</p>
+				</div>
+				<img
+					class="h-10"
+					src="https://external-content.duckduckgo.com/ip3/fastapi.tiangolo.com.ico"
+					alt="FastAPI Logo"
+				/>
+			</a>
+		</li>
+		<li class="col-span-1 rounded-xl border border-primary shadow">
+			<a href="https://www.python.org/" class="flex items-center justify-between space-x-6 p-6">
+				<div class="flex-1">
+					<div class="flex items-center space-x-3">
+						<h3 class="text-sm font-medium text-background-on">Python</h3>
+						<span
+							class="inline-flex flex-shrink-0 items-center rounded-full bg-secondary px-1.5 py-0.5 text-xs font-medium text-secondary-on ring-1 ring-inset ring-green-600/20"
+							>Language</span
+						>
+					</div>
+					<p class="mt-1 w-full text-pretty text-left text-sm text-background-on/80">
+						Python is a versatile and powerful language that lets you work quickly and integrate
+						systems more effectively.
+					</p>
+				</div>
+				<img
+					class="h-10"
+					src="https://external-content.duckduckgo.com/ip3/www.python.org.ico"
+					alt="Python Logo"
+				/>
+			</a>
+		</li>
+	</ul>
 </section>
