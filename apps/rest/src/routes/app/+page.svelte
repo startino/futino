37d82af06ca1886c1ef2c234bdf8573346424dba
Jorge Lewis <31695089+antopiahk@users.noreply.github.com>
2024-04-01 13:49:06 +0800

    project is all setup. colors working but forced (i dont have a clue whats going on there haha)

---
+++ apps/rest/src/routes/app/+page.svelte
@@ -0,0 +1,1 @@
+<div class="text-primary-500 bg-accent text-xl">HEY</div>
--- apps/rest/tailwind.config.js
+++ apps/rest/tailwind.config.js
@@ -1,64 +1,76 @@
-import { fontFamily } from "tailwindcss/defaultTheme";
+import { fontFamily } from 'tailwindcss/defaultTheme';
+import typographyConfig from './theme/typography.cjs';
+import colorConfig from './theme/color.cjs';
+import { animationsConfig, keyframesConfig } from './theme/animation.cjs';
 
 /** @type {import('tailwindcss').Config} */
+
+const alpha = '<alpha-value>';
+
 const config = {
-	darkMode: ["class"],
-	content: ["./src/**/*.{html,js,svelte,ts}"],
-	safelist: ["dark"],
+	darkMode: ['class'],
+	content: ['./src/**/*.{html,js,svelte,ts}', './theme/*.{html,js,svelte,ts,ttf,cjs}'],
+	safelist: ['dark'],
 	theme: {
 		container: {
 			center: true,
-			padding: "2rem",
+			padding: '2rem',
 			screens: {
-				"2xl": "1400px"
+				'2xl': '1400px'
 			}
 		},
 		extend: {
-			colors: {
-				border: "hsl(var(--border) / <alpha-value>)",
-				input: "hsl(var(--input) / <alpha-value>)",
-				ring: "hsl(var(--ring) / <alpha-value>)",
-				background: "hsl(var(--background) / <alpha-value>)",
-				foreground: "hsl(var(--foreground) / <alpha-value>)",
-				primary: {
-					DEFAULT: "hsl(var(--primary) / <alpha-value>)",
-					foreground: "hsl(var(--primary-foreground) / <alpha-value>)"
-				},
-				secondary: {
-					DEFAULT: "hsl(var(--secondary) / <alpha-value>)",
-					foreground: "hsl(var(--secondary-foreground) / <alpha-value>)"
-				},
-				destructive: {
-					DEFAULT: "hsl(var(--destructive) / <alpha-value>)",
-					foreground: "hsl(var(--destructive-foreground) / <alpha-value>)"
-				},
-				muted: {
-					DEFAULT: "hsl(var(--muted) / <alpha-value>)",
-					foreground: "hsl(var(--muted-foreground) / <alpha-value>)"
-				},
-				accent: {
-					DEFAULT: "hsl(var(--accent) / <alpha-value>)",
-					foreground: "hsl(var(--accent-foreground) / <alpha-value>)"
-				},
-				popover: {
-					DEFAULT: "hsl(var(--popover) / <alpha-value>)",
-					foreground: "hsl(var(--popover-foreground) / <alpha-value>)"
-				},
-				card: {
-					DEFAULT: "hsl(var(--card) / <alpha-value>)",
-					foreground: "hsl(var(--card-foreground) / <alpha-value>)"
-				}
-			},
+			animation: animationsConfig,
+			keyframes: keyframesConfig,
+			colors: colorConfig,
 			borderRadius: {
-				lg: "var(--radius)",
-				md: "calc(var(--radius) - 2px)",
-				sm: "calc(var(--radius) - 4px)"
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)'
 			},
 			fontFamily: {
 				sans: [...fontFamily.sans]
-			}
+			},
+			typography: ({ colors }) => ({
+				...typographyConfig(colors, alpha),
+				...{
+					DEFAULT: {
+						css: {
+							h1: {
+								fontWeight: 'bold'
+							},
+							h2: {
+								fontWeight: 'normal'
+							},
+							h3: {
+								fontWeight: 'normal'
+							},
+							h4: {
+								fontWeight: 'normal'
+							},
+							h5: {
+								fontWeight: 'normal'
+							},
+
+							h6: {
+								fontWeight: 'normal'
+							},
+							p: {
+								letterSpacing: '0.5',
+								lineHeight: '1.5',
+								opacity: '0.75'
+							}
+						}
+					}
+				}
+			})
 		}
 	},
+	plugins: [
+		require('@tailwindcss/forms'),
+		require('@tailwindcss/typography'),
+		require('@tailwindcss/aspect-ratio')
+	]
 };
 
 export default config;
