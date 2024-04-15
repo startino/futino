67d72f9803aa4cc2bc15cd0769ca4a875264c805
Nazif Barassounon <57366738+nazifbara@users.noreply.github.com>
2024-04-15 17:55:02 +0100

    rework navigation (#995)
    * rework navigation
    
    * delete old nav items

--- apps/oak/src/routes/app/+page.svelte
+++ apps/oak/src/routes/app/+page.svelte
@@ -1,11 +1,1 @@
-<script lang="ts">
-	import { superForm } from 'sveltekit-superforms/client';
-	import type { PageData } from './$types';
-	import * as Card from '$lib/components/ui/card';
-	import { Button } from '$lib/components/ui/button';
-	import AuthForm from '$lib/components/molecules/AuthForm.svelte';
-
-	export let data: PageData;
-</script>
-
-<h1>{data.profile?.full_name}</h1>
+<h1 class="text-3xl">Home</h1>
--- apps/oak/src/routes/app/company-account/+page.svelte
+++
@@ -1,1 +0,0 @@
-<h1 class="text-sm">Company Account</h1>
