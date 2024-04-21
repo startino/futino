789d495009e99882b1aa07adc79a95089d0cd7c0
Nazif Barassounon <57366738+nazifbara@users.noreply.github.com>
2024-04-21 21:02:05 +0100

    allow user to register

--- apps/oak/src/routes/(marketing)/register/+page.server.ts
+++ apps/oak/src/routes/(marketing)/register/+page.server.ts
@@ -1,6 +1,6 @@
 import { registrationSchema } from '$lib/schemas';
-import { fail, redirect } from '@sveltejs/kit';
-import { message, superValidate } from 'sveltekit-superforms/server';
+import { fail, redirect, type RecursiveRequired } from '@sveltejs/kit';
+import { setError, superValidate } from 'sveltekit-superforms/server';
 import { zod } from 'sveltekit-superforms/adapters';
 
 export const load = async () => {
@@ -11,12 +11,45 @@ export const load = async () => {
 	};
 };
 
-export const actions = async ({ request }) => {
-	const form = await superValidate(request, zod(registrationSchema));
+export const actions = {
+	default: async ({ request, locals: { apiClient } }) => {
+		const form = await superValidate(request, zod(registrationSchema));
 
-	if (!form.valid) {
-		return fail(400, { form });
+		if (!form.valid) {
+			return fail(400, { form });
+		}
+
+		type FormData = RecursiveRequired<typeof form.data>;
+
+		const formData: FormData = form.data as FormData;
+
+		const { data: org, error: orgErr } = await apiClient.supabase
+			.from('organizations')
+			.insert({ name: formData.organization.name })
+			.select()
+			.single();
+
+		if (orgErr) {
+			return setError(form, 'Something went wrong. Please, try again', { status: 500 });
+		}
+
+		const userData = formData.user;
+		const { error } = await apiClient.supabase.auth.signUp({
+			email: userData.email,
+			password: userData.password,
+			options: {
+				data: {
+					organization_id: org.id,
+					role: 'admin',
+					full_name: formData.user.fullName
+				}
+			}
+		});
+
+		if (error) {
+			return setError(form, 'Something went wrong. Please, try again', { status: 500 });
+		}
+
+		redirect(302, '/login?status=confirmation-sent');
 	}
-
-	message(form, 'Success!');
 };
