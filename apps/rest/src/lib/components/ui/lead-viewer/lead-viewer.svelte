673001462d3345b04e5ba11f7fba46923026f452
antopiahk <antopiahk@gmail.com>
2024-04-21 14:21:58 +0200

    fixing reactivity of comment text

--- apps/rest/src/lib/components/ui/lead-viewer/lead-viewer.svelte
+++ apps/rest/src/lib/components/ui/lead-viewer/lead-viewer.svelte
@@ -46,8 +46,8 @@
 		return subreddit;
 	}
 
-	$: reasonTextValue = 'Post is irrelevant because';
-	$: commentTextValue = lead?.comment ?? '';
+	let reasonTextValue = 'Post is irrelevant because';
+	$: commentTextValue = lead?.comment;
 
 	async function handlePublishComment() {
 		console.log('Trying to publish');
