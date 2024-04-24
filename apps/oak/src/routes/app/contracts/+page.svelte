07060d170818929a465e89254c25a55f5af4d0f1
Nazif Barassounon <57366738+nazifbara@users.noreply.github.com>
2024-04-24 18:02:08 +0100

    show contract entry to employees only

--- apps/oak/src/routes/app/contracts/+page.svelte
+++ apps/oak/src/routes/app/contracts/+page.svelte
@@ -105,253 +105,258 @@
 			userPendingApprovalsCount={userPending.length}
 		>
 			<svelte:fragment slot="entry-form">
-				<Dialog.Root bind:open={formOpen} onOpenChange={(o) => (formOpen = o)}>
-					<Dialog.Trigger>
-						<Button><Plus />Add</Button>
-					</Dialog.Trigger>
+				{#if $currentProfile.role === 'employee'}
+					<Dialog.Root bind:open={formOpen} onOpenChange={(o) => (formOpen = o)}>
+						<Dialog.Trigger>
+							<Button><Plus />Add</Button>
+						</Dialog.Trigger>
 
-					<Dialog.Content
-						transition={(node) => fly(node, { x: 300, duration: 300 })}
-						class="left-auto right-0 max-h-screen w-full max-w-lg translate-x-1 overflow-y-scroll border-y-0 sm:rounded-none"
-					>
-						<Dialog.Header>
-							<Dialog.Title>Add a contract</Dialog.Title>
-						</Dialog.Header>
+						<Dialog.Content
+							transition={(node) => fly(node, { x: 300, duration: 300 })}
+							class="left-auto right-0 max-h-screen w-full max-w-lg translate-x-1 overflow-y-scroll border-y-0 sm:rounded-none"
+						>
+							<Dialog.Header>
+								<Dialog.Title>Add a contract</Dialog.Title>
+							</Dialog.Header>
 
-						<form method="POST" enctype="multipart/form-data" use:enhance>
-							<input name="organization_id" hidden value={$formData.organization_id} />
-							<input name="owner_id" hidden value={$formData.owner_id} />
-							<input name="current_approver_id" hidden value={$formData.current_approver_id} />
+							<form method="POST" enctype="multipart/form-data" use:enhance>
+								<input name="organization_id" hidden value={$formData.organization_id} />
+								<input name="owner_id" hidden value={$formData.owner_id} />
+								<input name="current_approver_id" hidden value={$formData.current_approver_id} />
 
-							<Form.Field {form} name="vendor_id">
-								<Form.Control>
-									<Form.Label>Vendor</Form.Label>
-									<div>
-										<Combobox
-											placeholder="Select a vendor"
-											items={$vendors.map((v) => ({ label: v.name, value: v.id }))}
-											bind:value={$formData.vendor_id}
-											setValue={(value) => {
-												$formData.vendor_id = value;
-												selectedVendorId = value;
-											}}
-										/>
-									</div>
-								</Form.Control>
-								<Form.FieldErrors />
-							</Form.Field>
-							<Form.Field {form} name="parent_contract_id">
-								<Form.Control let:attrs>
-									<Form.Label>Parent contract</Form.Label>
-									<div>
-										{#if parentContracts[0]}
-											<Combobox
-												placeholder="Select a parent contract"
-												items={[
-													{ label: 'None', value: undefined },
-													...parentContracts.map((c) => ({
-														label: `#${c.number} | ${c.vendor.name}`,
-														value: c.id
-													}))
-												]}
-												bind:value={$formData.parent_contract_id}
-												setValue={(value) => ($formData.parent_contract_id = value)}
-												{...attrs}
-											/>
-										{:else}
-											<p class="text-xs font-bold text-accent">No parent contract</p>
-										{/if}
-									</div>
-								</Form.Control>
-								<Form.FieldErrors />
-							</Form.Field>
-							<Form.Field {form} name="account_id">
-								<Form.Control let:attrs>
-									<Form.Label>Account number</Form.Label>
-									<div>
-										<Combobox
-											placeholder="Select an account number"
-											items={$accounts.map((c) => ({ label: c.number.toString(), value: c.id }))}
-											bind:value={$formData.account_id}
-											{...attrs}
-											setValue={(value) => ($formData.account_id = value)}
-										/>
-									</div>
-								</Form.Control>
-								<Form.FieldErrors />
-							</Form.Field>
-							<Form.Field {form} name="spend_category_id">
-								<Form.Control let:attrs>
-									<Form.Label>Spend category</Form.Label>
-									<div>
-										<Combobox
-											placeholder="Select a spend category"
-											items={$spendCategories.map((c) => ({ label: c.name, value: c.id }))}
-											bind:value={$formData.spend_category_id}
-											{...attrs}
-											setValue={(value) => ($formData.spend_category_id = value)}
-										/>
-									</div>
-								</Form.Control>
-								<Form.FieldErrors />
-							</Form.Field>
-							<Form.Field {form} name="department_id">
-								<Form.Control let:attrs>
-									<Form.Label>Department</Form.Label>
-									<div>
-										<Combobox
-											placeholder="Select a department"
-											items={$departments.map((d) => ({ label: d.name, value: d.id }))}
-											bind:value={$formData.department_id}
-											{...attrs}
-											setValue={(value) => ($formData.department_id = value)}
-										/>
-									</div>
-								</Form.Control>
-								<Form.FieldErrors />
-							</Form.Field>
-							<Form.Field {form} name="project_id">
-								<Form.Control let:attrs>
-									<Form.Label>Project</Form.Label>
-									<div>
-										<Combobox
-											placeholder="Select a project"
-											items={$projects.map((d) => ({ label: d.name, value: d.id }))}
-											bind:value={$formData.project_id}
-											{...attrs}
-											setValue={(value) => ($formData.project_id = value)}
-										/>
-									</div>
-								</Form.Control>
-								<Form.FieldErrors />
-							</Form.Field>
-
-							<Form.Field {form} name="number">
-								<Form.Control let:attrs>
-									<Form.Label>Contract number</Form.Label>
-									<Input type="number" {...attrs} bind:value={$formData.number} />
-								</Form.Control>
-								<Form.FieldErrors />
-							</Form.Field>
-
-							<Form.Field {form} name="description">
-								<Form.Control let:attrs>
-									<Form.Label>Description</Form.Label>
-									<Textarea {...attrs} bind:value={$formData.description} />
-								</Form.Control>
-								<Form.FieldErrors />
-							</Form.Field>
-
-							<Form.Field {form} name="amount">
-								<Form.Control let:attrs>
-									<Form.Label>Amount</Form.Label>
-									<Input type="number" {...attrs} bind:value={$formData.amount} />
-								</Form.Control>
-								<Form.FieldErrors />
-							</Form.Field>
-
-							<Form.Field {form} name="start_date">
-								<Form.Control let:attrs>
-									<Form.Label>Start Date</Form.Label>
-									<input hidden bind:value={$formData.start_date} {...attrs} />
-									<Popover.Root>
+								<Form.Field {form} name="vendor_id">
+									<Form.Control>
+										<Form.Label>Vendor</Form.Label>
 										<div>
-											<Popover.Trigger
-												class={cn(
-													buttonVariants({ variant: 'outline' }),
-													'w-[220px] justify-start pl-4 text-left font-normal',
-													!startDateValue && 'text-muted-foreground'
-												)}
-											>
-												{startDateValue
-													? df.format(startDateValue.toDate(getLocalTimeZone()))
-													: 'Pick a date'}
-												<CalendarIcon class="ml-auto h-4 w-4 opacity-50" />
-											</Popover.Trigger>
-										</div>
-										<Popover.Content class="w-auto p-0" side="right">
-											<DatePicker
-												bind:value={startDateValue}
-												bind:placeholder={startDatePlaceholder}
+											<Combobox
+												placeholder="Select a vendor"
+												items={$vendors.map((v) => ({ label: v.name, value: v.id }))}
+												bind:value={$formData.vendor_id}
+												setValue={(value) => {
+													$formData.vendor_id = value;
+													selectedVendorId = value;
+												}}
 											/>
-										</Popover.Content>
-									</Popover.Root>
-								</Form.Control>
-							</Form.Field>
+										</div>
+									</Form.Control>
+									<Form.FieldErrors />
+								</Form.Field>
+								<Form.Field {form} name="parent_contract_id">
+									<Form.Control let:attrs>
+										<Form.Label>Parent contract</Form.Label>
+										<div>
+											{#if parentContracts[0]}
+												<Combobox
+													placeholder="Select a parent contract"
+													items={[
+														{ label: 'None', value: undefined },
+														...parentContracts.map((c) => ({
+															label: `#${c.number} | ${c.vendor.name}`,
+															value: c.id
+														}))
+													]}
+													bind:value={$formData.parent_contract_id}
+													setValue={(value) => ($formData.parent_contract_id = value)}
+													{...attrs}
+												/>
+											{:else}
+												<p class="text-xs font-bold text-accent">No parent contract</p>
+											{/if}
+										</div>
+									</Form.Control>
+									<Form.FieldErrors />
+								</Form.Field>
+								<Form.Field {form} name="account_id">
+									<Form.Control let:attrs>
+										<Form.Label>Account number</Form.Label>
+										<div>
+											<Combobox
+												placeholder="Select an account number"
+												items={$accounts.map((c) => ({ label: c.number.toString(), value: c.id }))}
+												bind:value={$formData.account_id}
+												{...attrs}
+												setValue={(value) => ($formData.account_id = value)}
+											/>
+										</div>
+									</Form.Control>
+									<Form.FieldErrors />
+								</Form.Field>
+								<Form.Field {form} name="spend_category_id">
+									<Form.Control let:attrs>
+										<Form.Label>Spend category</Form.Label>
+										<div>
+											<Combobox
+												placeholder="Select a spend category"
+												items={$spendCategories.map((c) => ({ label: c.name, value: c.id }))}
+												bind:value={$formData.spend_category_id}
+												{...attrs}
+												setValue={(value) => ($formData.spend_category_id = value)}
+											/>
+										</div>
+									</Form.Control>
+									<Form.FieldErrors />
+								</Form.Field>
+								<Form.Field {form} name="department_id">
+									<Form.Control let:attrs>
+										<Form.Label>Department</Form.Label>
+										<div>
+											<Combobox
+												placeholder="Select a department"
+												items={$departments.map((d) => ({ label: d.name, value: d.id }))}
+												bind:value={$formData.department_id}
+												{...attrs}
+												setValue={(value) => ($formData.department_id = value)}
+											/>
+										</div>
+									</Form.Control>
+									<Form.FieldErrors />
+								</Form.Field>
+								<Form.Field {form} name="project_id">
+									<Form.Control let:attrs>
+										<Form.Label>Project</Form.Label>
+										<div>
+											<Combobox
+												placeholder="Select a project"
+												items={$projects.map((d) => ({ label: d.name, value: d.id }))}
+												bind:value={$formData.project_id}
+												{...attrs}
+												setValue={(value) => ($formData.project_id = value)}
+											/>
+										</div>
+									</Form.Control>
+									<Form.FieldErrors />
+								</Form.Field>
 
-							<Form.Field {form} name="end_date">
-								<Form.Control>
-									<Form.Label>End Date</Form.Label>
-									<Popover.Root>
-										<Form.Control let:attrs>
-											<input hidden bind:value={$formData.end_date} {...attrs} />
+								<Form.Field {form} name="number">
+									<Form.Control let:attrs>
+										<Form.Label>Contract number</Form.Label>
+										<Input type="number" {...attrs} bind:value={$formData.number} />
+									</Form.Control>
+									<Form.FieldErrors />
+								</Form.Field>
+
+								<Form.Field {form} name="description">
+									<Form.Control let:attrs>
+										<Form.Label>Description</Form.Label>
+										<Textarea {...attrs} bind:value={$formData.description} />
+									</Form.Control>
+									<Form.FieldErrors />
+								</Form.Field>
+
+								<Form.Field {form} name="amount">
+									<Form.Control let:attrs>
+										<Form.Label>Amount</Form.Label>
+										<Input type="number" {...attrs} bind:value={$formData.amount} />
+									</Form.Control>
+									<Form.FieldErrors />
+								</Form.Field>
+
+								<Form.Field {form} name="start_date">
+									<Form.Control let:attrs>
+										<Form.Label>Start Date</Form.Label>
+										<input hidden bind:value={$formData.start_date} {...attrs} />
+										<Popover.Root>
 											<div>
 												<Popover.Trigger
 													class={cn(
 														buttonVariants({ variant: 'outline' }),
 														'w-[220px] justify-start pl-4 text-left font-normal',
-														!endDateValue && 'text-muted-foreground'
+														!startDateValue && 'text-muted-foreground'
 													)}
 												>
-													{endDateValue
-														? df.format(endDateValue.toDate(getLocalTimeZone()))
+													{startDateValue
+														? df.format(startDateValue.toDate(getLocalTimeZone()))
 														: 'Pick a date'}
 													<CalendarIcon class="ml-auto h-4 w-4 opacity-50" />
 												</Popover.Trigger>
 											</div>
-										</Form.Control>
-										<Popover.Content class="w-auto p-0" side="right">
-											<DatePicker bind:value={endDateValue} bind:placeholder={endDatePlaceholder} />
-										</Popover.Content>
-									</Popover.Root>
-									<Form.FieldErrors />
-								</Form.Control>
-							</Form.Field>
+											<Popover.Content class="w-auto p-0" side="right">
+												<DatePicker
+													bind:value={startDateValue}
+													bind:placeholder={startDatePlaceholder}
+												/>
+											</Popover.Content>
+										</Popover.Root>
+									</Form.Control>
+								</Form.Field>
 
-							<Form.Field {form} name="attachment">
-								<Form.Control let:attrs>
-									<Form.Label class="cursor-pointer">
-										<span class="mb-2 block">Attachment</span>
-										<div
-											class="flex w-fit items-center justify-start gap-4 rounded-md border border-input px-3 py-2"
-										>
-											{#if fileName}
-												{fileName}
-											{:else}
-												Upload PDF
-											{/if}
-											<Paperclip />
-										</div>
-										<input
-											hidden
-											type="file"
-											accept="application/pdf"
-											name="attachment"
-											on:input={(e) => (fileName = e.currentTarget.files[0].name)}
-											bind:files={$file}
-											{...attrs}
-											class="hidden"
-										/>
-									</Form.Label>
+								<Form.Field {form} name="end_date">
+									<Form.Control>
+										<Form.Label>End Date</Form.Label>
+										<Popover.Root>
+											<Form.Control let:attrs>
+												<input hidden bind:value={$formData.end_date} {...attrs} />
+												<div>
+													<Popover.Trigger
+														class={cn(
+															buttonVariants({ variant: 'outline' }),
+															'w-[220px] justify-start pl-4 text-left font-normal',
+															!endDateValue && 'text-muted-foreground'
+														)}
+													>
+														{endDateValue
+															? df.format(endDateValue.toDate(getLocalTimeZone()))
+															: 'Pick a date'}
+														<CalendarIcon class="ml-auto h-4 w-4 opacity-50" />
+													</Popover.Trigger>
+												</div>
+											</Form.Control>
+											<Popover.Content class="w-auto p-0" side="right">
+												<DatePicker
+													bind:value={endDateValue}
+													bind:placeholder={endDatePlaceholder}
+												/>
+											</Popover.Content>
+										</Popover.Root>
+										<Form.FieldErrors />
+									</Form.Control>
+								</Form.Field>
 
-									<Form.FieldErrors />
-								</Form.Control>
-							</Form.Field>
+								<Form.Field {form} name="attachment">
+									<Form.Control let:attrs>
+										<Form.Label class="cursor-pointer">
+											<span class="mb-2 block">Attachment</span>
+											<div
+												class="flex w-fit items-center justify-start gap-4 rounded-md border border-input px-3 py-2"
+											>
+												{#if fileName}
+													{fileName}
+												{:else}
+													Upload PDF
+												{/if}
+												<Paperclip />
+											</div>
+											<input
+												hidden
+												type="file"
+												accept="application/pdf"
+												name="attachment"
+												on:input={(e) => (fileName = e.currentTarget.files[0].name)}
+												bind:files={$file}
+												{...attrs}
+												class="hidden"
+											/>
+										</Form.Label>
 
-							<Form.Button type="submit" disabled={$submitting} class="my-4 w-full">
-								{#if $submitting}
-									<Loader2 class="animate-spin" />
-								{:else}
-									Submit
+										<Form.FieldErrors />
+									</Form.Control>
+								</Form.Field>
+
+								<Form.Button type="submit" disabled={$submitting} class="my-4 w-full">
+									{#if $submitting}
+										<Loader2 class="animate-spin" />
+									{:else}
+										Submit
+									{/if}
+								</Form.Button>
+
+								{#if $errors._errors}
+									<p class="text-sm text-destructive">{$errors._errors[0]}</p>
 								{/if}
-							</Form.Button>
-
-							{#if $errors._errors}
-								<p class="text-sm text-destructive">{$errors._errors[0]}</p>
-							{/if}
-						</form>
-					</Dialog.Content>
-				</Dialog.Root>
+							</form>
+						</Dialog.Content>
+					</Dialog.Root>
+				{/if}
 			</svelte:fragment>
 		</DataTable>
 	{/key}
