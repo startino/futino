f16630ae8396aa36c1b38a64246c1f7fda8993b0
Nazif Barassounon <57366738+nazifbara@users.noreply.github.com>
2024-04-24 18:07:44 +0100

    Feature / oak contract entry (#1075)
    * get rid of the getSession warning
    
    * add the contract entry form
    
    * make the contract form dialog act like a drawer
    
    * show contract entry to employees only

--- apps/oak/src/routes/app/subscription/+layout.server.ts
+++
@@ -1,6 +0,0 @@
-export const load = async ({ locals: { apiClient } }) => {
-	const paymentMethod = await apiClient.getUserBillingMethod();
-	return {
-		paymentMethod: paymentMethod ? { card: paymentMethod.card, id: paymentMethod.id } : null
-	};
-};
