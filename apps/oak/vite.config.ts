99ef1f6d0b4d901bc32cab01ff3a3090e97d6296
Nazif Barassounon <57366738+nazifbara@users.noreply.github.com>
2024-04-26 15:40:40 +0100

    fix build error

--- apps/oak/vite.config.ts
+++ apps/oak/vite.config.ts
@@ -3,9 +3,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	optimizeDeps: {
-		esbuildOptions: {
-			target: 'esnext'
-		}
+	build: {
+		target: 'ES2022'
 	}
 });
