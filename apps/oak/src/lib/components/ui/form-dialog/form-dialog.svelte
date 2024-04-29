80c5131f6be044dfef3750611135eb2daf41f134
Nazif Barassounon <57366738+nazifbara@users.noreply.github.com>
2024-04-29 10:57:52 +0100

    extract the FormDialog component

---
+++ apps/oak/src/lib/components/ui/form-dialog/form-dialog.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { Plus } from 'lucide-svelte';
+	import { fly } from 'svelte/transition';
+
+	import * as Dialog from '$lib/components/ui/dialog';
+	import { Button } from '$lib/components/ui/button';
+
+	export let title = '';
+	export let triggerLabel = 'Add';
+	export let open = false;
+</script>
+
+<Dialog.Root bind:open onOpenChange={(o) => (open = o)}>
+	<Dialog.Trigger>
+		<Button><Plus />{triggerLabel}</Button>
+	</Dialog.Trigger>
+
+	<Dialog.Content
+		transition={(node) => fly(node, { x: 300, duration: 300 })}
+		class="left-auto right-0 max-h-screen w-full max-w-lg translate-x-1 overflow-y-scroll border-y-0 sm:rounded-none"
+	>
+		<Dialog.Header>
+			<Dialog.Title>{title}</Dialog.Title>
+		</Dialog.Header>
+
+		<slot />
+	</Dialog.Content>
+</Dialog.Root>
