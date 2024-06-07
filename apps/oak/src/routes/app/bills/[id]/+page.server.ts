d8b4a632ce2b2e2dc5491e1b318d309580e35013
Nazif Barassounon <57366738+nazifbara@users.noreply.github.com>
2024-06-07 07:33:31 +0100

    oak bill rejection (#1306)
    * approver can reject a bill
    
    * show bill rejection note
    
    * fix report's bills filtering

--- apps/oak/src/routes/app/bills/[id]/+page.server.ts
+++ apps/oak/src/routes/app/bills/[id]/+page.server.ts
@@ -1,10 +1,10 @@
-import { error, fail } from '@sveltejs/kit';
+import { error, fail, type RecursiveRequired } from '@sveltejs/kit';
 import { setError, superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
 import { today } from '@internationalized/date';
 
 import { getBillById } from '$lib/server/db';
-import { billApprovalSchema } from '$lib/schemas';
+import { billApprovalSchema, billRejectionSchema } from '$lib/schemas';
 import { sendEmailNotif } from '$lib/utils.js';
 import { PUBLIC_SITE_URL } from '$env/static/public';
 
@@ -21,21 +21,21 @@ export const load = async ({ locals: { supabase }, params }) => {
 		data: { signedUrl }
 	} = await supabase.storage.from('invoices').createSignedUrl(bill.attachment, 60 * 60 * 24);
 
-	const form = await superValidate(zod(billApprovalSchema));
+	const approvalForm = await superValidate(zod(billApprovalSchema));
+	const rejectionForm = await superValidate(zod(billRejectionSchema));
 
-	return { bill, attachmentUrl: signedUrl, form };
+	return { bill, attachmentUrl: signedUrl, approvalForm, rejectionForm };
 };
 
 export const actions = {
 	approve: async ({ request, locals: { currentProfile, supabase, smtpTransporter } }) => {
-		const form = await superValidate(request, zod(billApprovalSchema));
+		const approvalForm = await superValidate(request, zod(billApprovalSchema));
 
-		if (!form.valid) {
-			return fail(400, { form });
+		if (!approvalForm.valid) {
+			return fail(400, { approvalForm });
 		}
 
-		const formData = form.data;
-		console.log({ formData });
+		const formData = approvalForm.data;
 
 		const { data: bill, error } = await supabase
 			.from('bills')
@@ -46,7 +46,7 @@ export const actions = {
 		if (error) {
 			console.log({ error });
 
-			return setError(form, 'Unable to approve this bill. Please try again', {
+			return setError(approvalForm, 'Unable to approve this bill. Please try again', {
 				status: 500
 			});
 		}
@@ -64,12 +64,12 @@ export const actions = {
 			if (updateError) {
 				console.log({ updateError });
 
-				return setError(form, 'Unable to approve this bill. Please try again', {
+				return setError(approvalForm, 'Unable to approve this bill. Please try again', {
 					status: 500
 				});
 			}
 
-			sendEmailNotif('validated-entry', {
+			sendEmailNotif('entry-validation', {
 				client: supabase,
 				smtp: smtpTransporter,
 				subject: 'Bill approved',
@@ -81,7 +81,71 @@ export const actions = {
 				}
 			});
 
-			return { form };
+			return { approvalForm };
+		}
+	},
+	reject: async ({ request, locals: { currentProfile, supabase, smtpTransporter } }) => {
+		const rejectionForm = await superValidate(request, zod(billRejectionSchema));
+
+		if (!rejectionForm.valid) {
+			return fail(400, { rejectionForm });
+		}
+
+		const formData = rejectionForm.data as RecursiveRequired<typeof rejectionForm.data>;
+
+		const { data: bill, error } = await supabase
+			.from('bills')
+			.select()
+			.eq('id', formData.bill_id)
+			.single();
+
+		if (error) {
+			console.log({ error });
+			return setError(rejectionForm, 'Unable to reject this bill. Please try again', {
+				status: 500
+			});
+		}
+
+		if (bill.approver_id === currentProfile.id || currentProfile.roles.includes('signer')) {
+			const { error: updateError } = await supabase
+				.from('bills')
+				.update({
+					status: 'rejected'
+				})
+				.eq('id', bill.id);
+
+			if (updateError) {
+				console.log({ updateError });
+
+				return setError(rejectionForm, 'Unable to reject this bill. Please try again', {
+					status: 500
+				});
+			}
+
+			const { error: rejectionError } = await supabase
+				.from('bill_rejections')
+				.insert({ creator_id: currentProfile.id, ...formData });
+
+			if (rejectionError) {
+				console.log({ rejectionError });
+				return setError(rejectionForm, 'Unable to reject this bill. Please try again', {
+					status: 500
+				});
+			}
+
+			sendEmailNotif('entry-validation', {
+				client: supabase,
+				smtp: smtpTransporter,
+				subject: 'Bill rejected',
+				receiverProfileId: bill.creator_id,
+				context: {
+					entryName: 'bill',
+					link: { url: `${PUBLIC_SITE_URL}/app/bills/${bill.id}`, label: 'View Bill' },
+					action: 'rejected'
+				}
+			});
+
+			return { rejectionForm };
 		}
 	}
 };
