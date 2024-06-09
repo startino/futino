35c403faf22965b33a028ec1be864956eb55a5ef
antopiahk <antopiahk@gmail.com>
2024-06-09 22:52:39 +0700

    fixed bug and testing tsparticles

--- apps/startino/src/routes/+layout.svelte
+++ apps/startino/src/routes/+layout.svelte
@@ -41,17 +41,13 @@
 <Header elements={headerLinks} />
 
 <main
-	class="relative min-h-screen w-full overflow-hidden bg-background text-center text-background-on"
+	class="relative min-h-screen w-full bg-background text-center text-background-on"
 >
-	<!-- The background pattern -->
-	<div
-		class="pointer-events-none absolute h-[50000px] w-full bg-inherit bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:150px_150px]"
-	></div>
 
 	<!-- The content -->
-	<arcitle class="relative flex min-h-screen w-full flex-col items-center justify-center gap-24">
+	<div class="relative flex min-h-screen w-full flex-col items-center justify-center gap-24">
 		<slot />
-	</arcitle>
+	</div >
 </main>
 
 <Footer links={footerLinks} />
---
+++ apps/startino/src/routes/particles/+page.svelte
@@ -0,0 +1,59 @@
+<script>
+    import Particles, { particlesInit } from '@tsparticles/svelte';
+    //import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+    import { loadSlim } from '@tsparticles/slim'; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+
+    let particlesUrl = 'http://foo.bar/particles.json'; // placeholder, replace it with a real url
+
+    let particlesConfig = {
+        particles: {
+            color: {
+                value: '#000'
+            },
+            links: {
+                enable: true,
+                color: '#000'
+            },
+            move: {
+                enable: true
+            },
+            number: {
+                value: 100
+            }
+        }
+    };
+
+    let onParticlesLoaded = (event) => {
+        const particlesContainer = event.detail.particles;
+
+        // you can use particlesContainer to call all the Container class
+        // (from the core library) methods like play, pause, refresh, start, stop
+    };
+
+    void particlesInit(async (engine) => {
+        // call this once per app
+        // you can use main to customize the tsParticles instance adding presets or custom shapes
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        //await loadFull(engine);
+        await loadSlim(engine);
+    });
+</script>
+
+<Particles
+        id="tsparticles"
+        class="put your classes here"
+        style=""
+        options="{particlesConfig}"
+        on:particlesLoaded="{onParticlesLoaded}"
+/>
+
+<!-- or -->
+
+<Particles
+        id="tsparticles"
+        class="put your classes here"
+        style=""
+        url="{particlesUrl}"
+        on:particlesLoaded="{onParticlesLoaded}"
+/>
