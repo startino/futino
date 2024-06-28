39b6b0ab7d8dd324c7f8ef5bb21c982af00d6287
Nazif Barassounon <57366738+nazifbara@users.noreply.github.com>
2024-06-28 10:06:53 +0100

    show monthly report snapshot

--- apps/oak/src/routes/app/report/+page.server.ts
+++ apps/oak/src/routes/app/report/+page.server.ts
@@ -3,11 +3,39 @@ import { error } from '@sveltejs/kit';
 import { getReportContracts } from '$lib/server/db/report';
 
 export const load = async ({ locals: { supabase, organization } }) => {
-	const { data, error: err } = await getReportContracts({ supabase, orgId: organization.id });
+	let snapshots: {url: string; label: string}[] = [];
 
-	if (err) {
-		console.log({ err });
+	try {
+		const { data, error: contractError } = await getReportContracts({ supabase, orgId: organization.id });
+
+		if (contractError) throw contractError;
+
+		const { data: snapshotList, error: listError } = await supabase.storage.from('report-snapshots').list(organization.id, {
+			limit: 100,
+			offset: 0,
+			sortBy: { column: 'name', order: 'desc' }
+		});
+
+		if(listError) throw listError;
+
+		for (const item of snapshotList) {
+			if (item.name === ".emptyFolderPlaceholder") {
+				continue;
+			}
+
+			const {
+				data: { signedUrl }
+			} = await supabase.storage
+				.from('report-snapshots')
+				.createSignedUrl(`${organization.id}/${item.name}`, 60 * 60 * 24 * 7);
+
+			snapshots.push({url: signedUrl, label: item.name.split(' ')[0]})
+		}
+
+		return { contracts: data, snapshots };
+	} catch (e) {
+		console.error("Error while getting report contracts");
 		return error(500, 'Something went wrong!');
 	}
-	return { contracts: data };
+
 };
--- apps/oak/src/routes/app/report/+page.svelte
+++ apps/oak/src/routes/app/report/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
-	import { CalendarDate, getLocalTimeZone, parseDate, today } from '@internationalized/date';
+	import { getLocalTimeZone, today } from '@internationalized/date';
 
 	import DataTable from './data-table.svelte';
 	import { getReportRows } from '$lib/utils';
 	import { DateInput } from '$lib/components/ui/date-input';
+	import { FormDialog } from '$lib/components/ui/form-dialog';
+	import { Button } from '$lib/components/ui/button';
+	import { Trigger } from '$lib/components/ui/dialog';
 
 	export let data;
 
@@ -14,8 +17,30 @@
 
 <h1 class="mb-10 text-3xl">Report</h1>
 
-<div class="mb-4">
+<div class="mb-4 flex items-center justify-between">
 	<DateInput bind:value={selectedPeriod} />
+
+	<FormDialog title="Monthly Report Snapshots">
+		<svelte:fragment slot="trigger">
+			<Trigger>
+				<Button>Show Report Snapshots</Button>
+			</Trigger>
+		</svelte:fragment>
+
+		{#if data.snapshots.length === 0}
+			<p>No report snapshots for now</p>
+		{:else}
+			<ul class=" grid list-disc gap-2 px-2">
+				{#each data.snapshots as snapshot}
+					<li>
+						<a href={snapshot.url} download class="text-primary hover:text-accent"
+							>{snapshot.label}</a
+						>
+					</li>
+				{/each}
+			</ul>
+		{/if}
+	</FormDialog>
 </div>
 
 {#key selectedPeriod}
