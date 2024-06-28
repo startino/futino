bd12d434d7e0c3f70847281f28480421f5eaa816
Nazif Barassounon <57366738+nazifbara@users.noreply.github.com>
2024-06-28 10:51:14 +0100

    oak report snapshot (#1362)
    * create the report-snapshot edge function
    
    * show monthly report snapshot

--- apps/oak/src/routes/app/report/+page.svelte
+++ apps/oak/src/routes/app/report/+page.svelte
@@ -1,59 +1,46 @@
 <script lang="ts">
-	import { CalendarDate, getLocalTimeZone, parseDate, today } from '@internationalized/date';
+	import { getLocalTimeZone, today } from '@internationalized/date';
 
 	import DataTable from './data-table.svelte';
-	import type { ReportContracts, ReportDataTableRow } from '$lib/types';
-	import { getMonthsDifference } from '$lib/utils';
+	import { getReportRows } from '$lib/utils';
 	import { DateInput } from '$lib/components/ui/date-input';
+	import { FormDialog } from '$lib/components/ui/form-dialog';
+	import { Button } from '$lib/components/ui/button';
+	import { Trigger } from '$lib/components/ui/dialog';
 
 	export let data;
 
 	let selectedPeriod = today(getLocalTimeZone());
-
-	const getReportRows = (data: ReportContracts, period: CalendarDate): ReportDataTableRow[] => {
-		return data
-			.filter((c) => period.compare(parseDate(c.start_date)) >= 0)
-			.map((c) => {
-				const billedAmount = c.bills
-					.filter(
-						(b) =>
-							b.posting_period &&
-							parseDate(b.posting_period).compare(period) <= 0 &&
-							b.status === 'approved'
-					)
-					.reduce((prev, curr) => prev + curr.amount, 0);
-
-				let elapsedMonths: number;
-
-				if (
-					period.compare(parseDate(c.end_date)) <= 0 &&
-					period.compare(parseDate(c.start_date)) >= 0
-				) {
-					elapsedMonths = getMonthsDifference(c.start_date, period.toString());
-				} else if (period.compare(parseDate(c.end_date)) > 0) {
-					elapsedMonths = getMonthsDifference(c.start_date, c.end_date);
-				} else {
-					elapsedMonths = 0;
-				}
-
-				const totalMonths = getMonthsDifference(c.start_date, c.end_date);
-
-				let accrualBalance = 0;
-
-				accrualBalance =
-					(elapsedMonths / (totalMonths === 0 ? 1 : totalMonths)) * c.amount - billedAmount;
-
-				return { ...c, billedAmount, accrualBalance, openAmount: c.amount - billedAmount };
-			});
-	};
 
 	$: rows = getReportRows(data.contracts, selectedPeriod);
 </script>
 
 <h1 class="mb-10 text-3xl">Report</h1>
 
-<div class="mb-4">
+<div class="mb-4 flex items-center justify-between">
 	<DateInput bind:value={selectedPeriod} />
+
+	<FormDialog title="Monthly Report Snapshots">
+		<svelte:fragment slot="trigger">
+			<Trigger>
+				<Button>Show Report Snapshots</Button>
+			</Trigger>
+		</svelte:fragment>
+
+		{#if data.snapshots.length === 0}
+			<p>No report snapshots for now</p>
+		{:else}
+			<ul class=" grid list-disc gap-2 px-2">
+				{#each data.snapshots as snapshot}
+					<li>
+						<a href={snapshot.url} download class="text-primary hover:text-accent"
+							>{snapshot.label}</a
+						>
+					</li>
+				{/each}
+			</ul>
+		{/if}
+	</FormDialog>
 </div>
 
 {#key selectedPeriod}
