0e7b1faf9488c36f9f5ae5a7756592c6d034b7b2
Nazif Barassounon <57366738+nazifbara@users.noreply.github.com>
2024-07-19 09:30:02 +0100

    only allow finance role to add bill

--- apps/oak/src/routes/app/bills/+page.svelte
+++ apps/oak/src/routes/app/bills/+page.svelte
@@ -2,13 +2,18 @@
 	import { Plus } from 'lucide-svelte';
 	import { toast } from 'svelte-sonner';
 
+	import * as AlertDialog from '$lib/components/ui/alert-dialog';
 	import DataTable from './data-table.svelte';
 	import { FormDialog } from '$lib/components/ui/form-dialog';
 	import * as Dialog from '$lib/components/ui/dialog';
 	import { Button } from '$lib/components/ui/button';
 	import BillForm from './bill-form.svelte';
+	import { getContext } from '$lib/utils';
 
 	export let data;
+
+	const iam = getContext('iam');
+	const currentProfile = getContext('currentProfile');
 
 	let formOpen = false;
 </script>
@@ -17,20 +22,61 @@
 {#key data.bills}
 	<DataTable data={data.bills}>
 		<svelte:fragment slot="entry-form">
-			<FormDialog bind:open={formOpen} title="Add bill">
-				<svelte:fragment slot="trigger">
-					<Dialog.Trigger>
+			{#if iam.isAllowedTo('bills.create') && !$currentProfile.approver_id}
+				<AlertDialog.Root>
+					<AlertDialog.Trigger>
 						<Button><Plus />Add</Button>
-					</Dialog.Trigger>
-				</svelte:fragment>
-				<BillForm
-					data={data.form}
-					onSuccess={() => {
-						toast.success('Bill successfully created!');
-						formOpen = false;
-					}}
-				/>
-			</FormDialog>
+					</AlertDialog.Trigger>
+					<AlertDialog.Content>
+						<AlertDialog.Header>
+							<AlertDialog.Title>Approver required</AlertDialog.Title>
+							<AlertDialog.Description>
+								To add a bill, reach out to your admin to get assigned an approver (supervisor).
+							</AlertDialog.Description>
+						</AlertDialog.Header>
+						<AlertDialog.Footer>
+							<AlertDialog.Action>OK</AlertDialog.Action>
+						</AlertDialog.Footer>
+					</AlertDialog.Content>
+				</AlertDialog.Root>
+			{/if}
+
+			{#if !iam.isAllowedTo('bills.create')}
+				<AlertDialog.Root>
+					<AlertDialog.Trigger>
+						<Button><Plus />Add</Button>
+					</AlertDialog.Trigger>
+					<AlertDialog.Content>
+						<AlertDialog.Header>
+							<AlertDialog.Title>Not allowed</AlertDialog.Title>
+							<AlertDialog.Description>
+								Only users with finance roles can add a bill. Please reach out to your admin to
+								update your role if necessary
+							</AlertDialog.Description>
+						</AlertDialog.Header>
+						<AlertDialog.Footer>
+							<AlertDialog.Action>OK</AlertDialog.Action>
+						</AlertDialog.Footer>
+					</AlertDialog.Content>
+				</AlertDialog.Root>
+			{/if}
+
+			{#if iam.isAllowedTo('bills.create')}
+				<FormDialog bind:open={formOpen} title="Add bill">
+					<svelte:fragment slot="trigger">
+						<Dialog.Trigger>
+							<Button><Plus />Add</Button>
+						</Dialog.Trigger>
+					</svelte:fragment>
+					<BillForm
+						data={data.form}
+						onSuccess={() => {
+							toast.success('Bill successfully created!');
+							formOpen = false;
+						}}
+					/>
+				</FormDialog>
+			{/if}
 		</svelte:fragment>
 	</DataTable>
 {/key}
