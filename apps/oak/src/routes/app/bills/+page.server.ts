66577ddfe99aba8259cd2c09083356ebe9879b00
Nazif Barassounon <57366738+nazifbara@users.noreply.github.com>
2024-08-04 15:57:05 +0100

    bill approval flow start with the contract owner

--- apps/oak/src/routes/app/bills/+page.server.ts
+++ apps/oak/src/routes/app/bills/+page.server.ts
@@ -4,10 +4,10 @@ import { zod } from 'sveltekit-superforms/adapters';
 
 import { billSchema } from '$lib/schemas';
 import { getBillDataTableRow } from '$lib/server/db/bills';
-import { findSigner, getInitialApprover } from '$lib/server/db/profiles';
+import { getInitialApprover } from '$lib/server/db/profiles';
 import { sendEmailNotif } from '$lib/utils';
 import { PUBLIC_SITE_URL } from '$env/static/public';
-import type { Tables } from '$lib/server/supabase.types';
+import type { Tables } from '$lib/server/supabase.types.js';
 
 export const load = async ({ locals: { organization, supabase } }) => {
 	const form = await superValidate(zod(billSchema));
@@ -44,15 +44,68 @@ export const actions = {
 
 		const path = `/${crypto.randomUUID()}-${formData.attachment.name}`;
 
-		const { error: uploadError } = await supabase.storage
-			.from('invoices')
-			.upload(path, formData.attachment, {
-				cacheControl: '3600',
-				upsert: false
-			});
+		try {
+			const { data: contract, error: contractError } = await supabase
+				.from('contracts')
+				.select('owner:profiles!contracts_owner_id_fkey (*)')
+				.eq('id', formData.contract_id)
+				.single();
 
-		if (uploadError) {
-			console.error(uploadError);
+			if (contractError) throw contractError;
+
+			const { error: uploadError } = await supabase.storage
+				.from('invoices')
+				.upload(path, formData.attachment, {
+					cacheControl: '3600',
+					upsert: false
+				});
+
+			if (uploadError) throw uploadError;
+
+			let approver: Tables<'profiles'> | null = null;
+
+			if (formData.amount <= contract.owner.approval_threshold) {
+				approver = contract.owner;
+			} else {
+				const { approver: initalApprover, error: approverError } = await getInitialApprover({
+					profile: contract.owner,
+					amount: formData.amount,
+					client: supabase
+				});
+
+				if (approverError) throw approverError;
+				approver = initalApprover;
+			}
+
+			const { data, error: billError } = await supabase
+				.from('bills')
+				.insert({
+					...formData,
+					organization_id: organization.id,
+					creator_id: currentProfile.id,
+					attachment: path,
+					approver_id: approver ? approver.id : null
+				})
+				.select()
+				.single();
+
+			if (billError) throw billError;
+
+			if (approver) {
+				sendEmailNotif('new-entry', {
+					subject: 'New Bill',
+					receiverProfileId: approver.id,
+					client: supabase,
+					smtp: smtpTransporter,
+					context: {
+						link: { url: `${PUBLIC_SITE_URL}/app/bills/${data.id}`, label: 'View bill' },
+						entryName: 'bill'
+					}
+				});
+			}
+		} catch (error) {
+			console.error(error);
+
 			return setError(
 				withFiles(form),
 				'attachment',
@@ -61,48 +114,6 @@ export const actions = {
 					status: 500
 				}
 			);
-		}
-
-		const { approver, error: approverError } = await getInitialApprover({
-			profile: currentProfile,
-			amount: formData.amount,
-			client: supabase
-		});
-
-		if (approverError) {
-			console.warn('approver error', { approverError });
-			return setError(withFiles(form), 'Unable to add contract. Please try again.', {
-				status: 500
-			});
-		}
-
-		const { data, error: BillError } = await supabase
-			.from('bills')
-			.insert({
-				...formData,
-				organization_id: organization.id,
-				creator_id: currentProfile.id,
-				attachment: path,
-				approver_id: approver ? approver.id : null
-			})
-			.select()
-			.single();
-
-		if (BillError) {
-			return setError(form, 'Unable to add the bill. Please try again', { status: 500 });
-		}
-
-		if (approver) {
-			sendEmailNotif('new-entry', {
-				subject: 'New Bill',
-				receiverProfileId: approver.id,
-				client: supabase,
-				smtp: smtpTransporter,
-				context: {
-					link: { url: `${PUBLIC_SITE_URL}/app/bills/${data.id}`, label: 'View bill' },
-					entryName: 'bill'
-				}
-			});
 		}
 
 		return message(form, 'Bill successfully added!');
